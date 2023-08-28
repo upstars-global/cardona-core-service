8ddb55ac92916dfc4d6a6762e022d655cc4d48dd
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2023-08-28 10:03:49 +0000

    BAC-1717 create base functionality

---
+++ src/components/templates/BaseList/_components/NameWithShortIdField.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import NameWithIdField from './NameWithIdField.vue'
+
+withDefaults(
+  defineProps<{
+    item: object
+    getUpdateRoute?: Function
+    isShowYou?: boolean
+  }>(),
+  {
+    getUpdateRoute: () => () => ({}),
+  }
+)
+</script>
+
+<template>
+  <name-with-id-field
+    :item="item"
+    :get-update-route="getUpdateRoute"
+    :is-show-you="isShowYou"
+    is-short
+  >
+    <slot></slot>
+  </name-with-id-field>
+</template>
