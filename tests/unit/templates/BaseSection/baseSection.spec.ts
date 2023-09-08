b0ce82475dc5858a43d784e394c933237b8d65dc
Andrey Danilov <a.danilov@upstars.com>
2023-09-08 09:42:56 +0000

    ALARO-1702 helpers for tests

--- tests/unit/templates/BaseSection/baseSection.spec.ts
+++ tests/unit/templates/BaseSection/baseSection.spec.ts
@@ -1,6 +1,5 @@
-import { createLocalVue, enableAutoDestroy, mount } from '@vue/test-utils'
+import { enableAutoDestroy, mount } from '@vue/test-utils'
 import VueRouter from 'vue-router'
-import BootstrapVue from 'bootstrap-vue'
 import { useDemoSection } from '../../../../src/pages/demo/useDemo'
 import BaseSection from '../../../../src/components/templates/BaseSection/index.vue'
 import { PageType } from '../../../../src/components/templates/BaseSection/model'
@@ -8,7 +7,7 @@ import flushPromises from 'flush-promises'
 import { getPermissionKeys } from '../../../../src/helpers'
 import { permissionPrefix } from '@productConfig'
 import ApiService from '../../../../src/services/api'
-
+import { createMockVue, generateTestSelector } from '../../../helpers/createLocalVue'
 enableAutoDestroy(afterEach)
 
 const demoListComponentName = 'DemoList'
@@ -25,45 +24,45 @@ ApiService.request = jest.fn(() => {
   }
 })
 
-const createButtonSelector = '[data-testid=create-button]'
-const stayButtonSelector = '[data-testid=stay-button]'
-const saveButtonSelector = '[data-testid=save-button]'
-const cancelButtonSelector = '[data-testid=cancel-button]'
-// @ts-ignore
-ApiService.request = jest.fn(() => {
-  return {
-    data: {
-      id: 'test-id',
-    },
-  }
-})
+const createButtonSelector = generateTestSelector('create-button')
+const stayButtonSelector = generateTestSelector('stay-button')
+const saveButtonSelector = generateTestSelector('save-button')
+const cancelButtonSelector = generateTestSelector('cancel-button')
+const findButtonElements = (wrapper) => {
+  const createButtonElement = wrapper.find(createButtonSelector)
+  const stayButtonElement = wrapper.find(stayButtonSelector)
+  const saveButtonElement = wrapper.find(saveButtonSelector)
+  const cancelButtonElement = wrapper.find(cancelButtonSelector)
+
+  return { createButtonElement, stayButtonElement, saveButtonElement, cancelButtonElement }
+}
+
 describe('BaseSection', () => {
-  beforeEach(() => {
-    localVue = createLocalVue()
-    localVue.use(VueRouter)
-    localVue.use(BootstrapVue)
-    router = new VueRouter({
-      mode: 'abstract',
-      base: process.env.BASE_URL,
-      routes: [
-        {
-          path: '/demo',
-          name: demoListComponentName,
-          component: {},
-        },
-        {
-          path: '/demo/update/:id',
-          name: demoUpdateComponentName,
-          component: {},
-        },
-      ],
-    })
-  })
-  afterEach(() => {
-    localVue = null
-    router = null
-  })
   describe('testing pageType props', () => {
+    beforeEach(() => {
+      localVue = createMockVue()
+      router = new VueRouter({
+        mode: 'abstract',
+        base: process.env.BASE_URL,
+        routes: [
+          {
+            path: '/demo',
+            name: demoListComponentName,
+            component: {},
+          },
+          {
+            path: '/demo/update/:id',
+            name: demoUpdateComponentName,
+            component: {},
+          },
+        ],
+      })
+    })
+    afterEach(() => {
+      localVue = null
+      router = null
+    })
+
     it('without set pageType', () => {
       const wrapper = mount(BaseSection, {
         propsData: {
@@ -72,10 +71,13 @@ describe('BaseSection', () => {
         router,
         localVue,
       })
-      expect(wrapper.find(createButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(stayButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(saveButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(cancelButtonSelector).exists()).toBe(false)
+
+      const { createButtonElement, stayButtonElement, saveButtonElement, cancelButtonElement } =
+        findButtonElements(wrapper)
+      expect(createButtonElement.exists()).toBe(false)
+      expect(stayButtonElement.exists()).toBe(false)
+      expect(saveButtonElement.exists()).toBe(false)
+      expect(cancelButtonElement.exists()).toBe(false)
     })
     it('set pageType created', () => {
       const wrapper = mount(BaseSection, {
@@ -86,10 +88,13 @@ describe('BaseSection', () => {
         router,
         localVue,
       })
-      expect(wrapper.find(createButtonSelector).exists()).toBe(true)
-      expect(wrapper.find(stayButtonSelector).exists()).toBe(true)
-      expect(wrapper.find(saveButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(cancelButtonSelector).exists()).toBe(true)
+
+      const { createButtonElement, stayButtonElement, saveButtonElement, cancelButtonElement } =
+        findButtonElements(wrapper)
+      expect(createButtonElement.exists()).toBe(true)
+      expect(stayButtonElement.exists()).toBe(true)
+      expect(saveButtonElement.exists()).toBe(false)
+      expect(cancelButtonElement.exists()).toBe(true)
     })
     it('set pageType update', () => {
       const wrapper = mount(BaseSection, {
@@ -100,10 +105,13 @@ describe('BaseSection', () => {
         router,
         localVue,
       })
-      expect(wrapper.find(createButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(stayButtonSelector).exists()).toBe(false)
-      expect(wrapper.find(saveButtonSelector).exists()).toBe(true)
-      expect(wrapper.find(cancelButtonSelector).exists()).toBe(true)
+
+      const { createButtonElement, stayButtonElement, saveButtonElement, cancelButtonElement } =
+        findButtonElements(wrapper)
+      expect(createButtonElement.exists()).toBe(false)
+      expect(stayButtonElement.exists()).toBe(false)
+      expect(saveButtonElement.exists()).toBe(true)
+      expect(cancelButtonElement.exists()).toBe(true)
     })
     it('correct routing on cansel click', async () => {
       const wrapper = mount(BaseSection, {
