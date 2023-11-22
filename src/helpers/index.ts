bd1c106855ed11b2e4c705d3bf706c605fcbb0c0
v.pecheritsya <v.pecheritsya@upstars.com>
2023-11-22 19:14:28 +0200

    BAC-2044: fix helper short id and add example with less symbol id

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -123,6 +123,7 @@ export const getPermissionKeys = (config: {
 }
 
 export const getShortString = (text: string | number, letterCount = 4): string => {
+  if (letterCount * 2 >= text.toString().length) return text.toString()
   const startString = String(text).slice(0, letterCount)
   const endString = String(text).slice(-letterCount)
   return `${startString}...${endString}`
