358713ab10a186fce1abe2df62bff13d5a7279b6
Andrey Danilov <a.danilov@upstars.com>
2024-05-30 12:19:42 +0000

    Resolve BAC-2663 "Fix core"

--- src/@model/templates/baseField/formDate.ts
+++ src/@model/templates/baseField/formDate.ts
@@ -1,14 +1,15 @@
 import type { Component } from 'vue'
 import type { TranslateResult } from 'vue-i18n'
+import { markRaw } from 'vue'
 import FormDateField from '../../../components/templates/FieldGenerator/_components/FormDateField.vue'
 import type { IBaseField } from './base'
 import { BaseField } from './base'
-import { markRaw } from 'vue'
 
 export interface IFormDateBaseField extends IBaseField {
   readonly value?: string | Date
   readonly buttonOnly?: boolean
   readonly dateFormat?: string
+  readonly config?: Record<string, unknown>
 }
 
 export class FormDateBaseField extends BaseField implements IFormDateBaseField {
@@ -17,12 +18,15 @@ export class FormDateBaseField extends BaseField implements IFormDateBaseField {
   readonly buttonOnly?: boolean
   readonly placeholder: TranslateResult
   readonly dateFormat: string
+  readonly config?: Record<string, unknown>
+
   constructor(field: IFormDateBaseField) {
     super(field)
     this._value = field.value
     this.buttonOnly = field.buttonOnly
     this.placeholder = field.placeholder ?? 'YYYY-MM-DD'
     this.dateFormat = field.dateFormat ?? 'Y.m.d'
+    this.config = field.config
   }
 
   transformField() {
