f4d985ff5704a6f60526ee7a24a4476563bdf17f
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2024-07-05 12:52:01 +0000

    BAC-2773: add base fix payouts list

--- src/configs/productConfig.ts
+++ src/configs/productConfig.ts
@@ -2,3 +2,4 @@ export const productName = 'demo'
 export const productId = 0
 export const permissionPrefix = 'demo'
 export const ApiTypePrefix = 'App.V2.'
+export const WSTypePrefix = 'app/v2/'
---
+++ src/services/ws/index.ts
@@ -0,0 +1,128 @@
+import { getAccessToken, getRefreshToken, isLoggedIn } from 'axios-jwt'
+import store from '../../store'
+import { messageTypes } from './config'
+import { WSTypePrefix } from '@productConfig'
+
+const DEFAULT_TIME_RECONNECT = 1000
+const RECONNECTION_TIME_MULTIPLIER = 2
+
+class WSService {
+  static ws: null | WebSocket = null
+  static WSListSubscribe: Set<string> = new Set()
+  static timeReconnect: number = DEFAULT_TIME_RECONNECT
+  static Channel = {
+    Ping: 'ping',
+  }
+
+  static readonly maxTimeReconnect: number = 60000
+
+  static async connect(channel) {
+    if (!isLoggedIn() && !channel)
+      return
+    this.Channel = channel
+
+    if (!getAccessToken())
+      return
+
+    const { accessToken } = await store.dispatch('authCore/refreshAuth', getRefreshToken())
+    const url = `wss://${location.host || location.hostname}/ws?jwt=${accessToken}`
+
+    this.ws = await new WebSocket(url)
+
+    this.ws.onopen = () => {
+      this.timeReconnect = DEFAULT_TIME_RECONNECT
+      this.sendObj([messageTypes.SUBSCRIBE, this.Channel.Ping])
+
+      if (this.WSListSubscribe.size) {
+        this.WSListSubscribe.forEach(event => {
+          this.send(event)
+        })
+      }
+    }
+
+    this.ws.onmessage = event => {
+      this.parseData(JSON.parse(event.data) as Array<any>)
+    }
+
+    this.ws.onclose = async event => {
+      this.ws?.close()
+      this.ws = null
+      if (
+        event.wasClean
+        && (this.timeReconnect !== DEFAULT_TIME_RECONNECT || event.code === 1000)
+      ) {
+        this.WSListSubscribe.clear()
+      }
+      else {
+        this.timeReconnect *= RECONNECTION_TIME_MULTIPLIER
+        if (this.timeReconnect > this.maxTimeReconnect)
+          this.timeReconnect = DEFAULT_TIME_RECONNECT
+        setTimeout(() => this.connect(channel), this.timeReconnect)
+      }
+      console.log(`Код: ${event.code} причина: ${event.reason}`)
+    }
+
+    this.ws.onerror = error => {
+      console.log(error)
+    }
+  }
+
+  static disconnect = () => {
+    this.ws?.close()
+  }
+
+  static send(text) {
+    if (!this.ws)
+      return
+
+    if (this.ws.readyState === 1)
+      this.ws.send(text)
+    else
+      setTimeout(() => this.send(text), DEFAULT_TIME_RECONNECT)
+  }
+
+  static sendObj(message) {
+    if (!this.ws)
+      return
+    this.send(JSON.stringify(message))
+  }
+
+  static subscribe(channel) {
+    if (!this.ws)
+      return
+
+    const message = JSON.stringify([messageTypes.SUBSCRIBE, WSTypePrefix + channel])
+
+    this.WSListSubscribe.add(message)
+    this.send(message)
+  }
+
+  static unsubscribe(channel) {
+    if (!this.ws)
+      return
+
+    const message = JSON.stringify([messageTypes.UNSUBSCRIBE, WSTypePrefix + channel])
+
+    this.WSListSubscribe.delete(message)
+    this.send(message)
+  }
+
+  static async parseData(messageArray: Array<any>) {
+    const [messageTypesData, channelDataSTR, data] = messageArray
+
+    // if(channelDataSTR === this.Channel.Ping) {
+    //   this.send('pong')
+    // }
+
+    if (messageTypesData === messageTypes.EVENT) {
+      const channelData = String(channelDataSTR).replace(WSTypePrefix, '')
+
+      if (channelData === 'ping')
+        return
+      await store.dispatch(`${channelData}/setWSData`, data)
+    }
+  }
+}
+
+export default WSService
+export { messageTypes }
