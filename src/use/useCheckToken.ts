000fcc0f4b273566e90cfaedcd33738c4fde967f
Vitalii <pecheritsa.vv@gmail.com>
2024-07-29 19:08:21 +0300

    BAC-3148: add base check token validation

--- src/use/useCheckToken.ts
+++ src/use/useCheckToken.ts
@@ -1,7 +1,6 @@
 import { computed, watch } from 'vue'
 import { useStore } from 'vuex'
 import { useStorage } from '@vueuse/core'
-import useToastService from '../helpers/toasts'
 
 export function useCheckToken() {
   const store = useStore()
@@ -9,28 +8,31 @@ export function useCheckToken() {
   const route = useRoute()
   const isLoginPage = computed(() => route.name === 'Login')
 
-  const tokenKey = ref<Ref<Record<string, unknown> | null>>(null)
+  const tokenKey = ref<string>('')
   const getActualTokenKey = () => Object.keys(localStorage).find(key => key.includes('auth-tokens-'))
-  const tokenState = ref(null)
-  const { toastError } = useToastService()
+  const tokenState = ref<Record<string, unknown> | null>(null)
+
+  const checkOnActualToken = async () => {
+    try {
+      await store.dispatch('fetchCurrentUser')
+    }
+    catch (e) {
+      await store.dispatch('authCore/clearAuth')
+      await router.push({ name: 'Login' })
+    }
+  }
 
   watch(() => tokenState.value, async () => {
     if (isLoginPage.value)
       return
-    try {
-      JSON.parse(localStorage[tokenKey.value])
-    }
-    catch (e) {
-      toastError('TOKEN_INVALID')
-      await store.dispatch('authCore/clearAuth')
-      await router.push({ name: 'Login' })
-    }
+
+    await checkOnActualToken()
   }, { deep: true })
 
   const initCheckToken = () => {
-    const tokenKey = getActualTokenKey()
+    tokenKey.value = getActualTokenKey()
 
-    tokenState.value = useStorage(tokenKey, JSON.parse(localStorage[tokenKey]))
+    tokenState.value = useStorage(tokenKey.value, JSON.parse(localStorage[tokenKey.value]))
   }
 
   return {
