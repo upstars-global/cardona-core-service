69c8b066bc704d090ff8bda4ec5226063c417b98
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-09-23 12:34:24 +0300

    BAC-2953: fix import path and add custom icon on success copy

---
+++ src/components/templates/_components/BtnTooltip.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { TranslateResult } from 'vue-i18n'
+import type { VColors, VSizes, VVariants } from '../../../@model/vuetify'
+import type { IconsList } from '../../../@model/enums/icons'
+
+interface Props {
+  tooltipText: TranslateResult
+  location?: string
+  size?: VSizes | string | number
+  color?: VColors
+  variant?: VVariants
+  appendIcon?: IconsList
+  prependIcon?: IconsList
+  label?: string
+}
+
+interface Emits {
+  (event: 'click')
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
+
+<template>
+  <VTooltip
+    :location="location"
+    :text="tooltipText"
+  >
+    <template #activator="{ props }">
+      <VBtn
+        :append-icon="appendIcon"
+        :prepend-icon="prependIcon"
+        :variant="variant"
+        :size="size"
+        :color="color"
+        v-bind="props"
+        @click="$emit('click')"
+      >
+        <slot>
+          {{ label }}
+        </slot>
+      </VBtn>
+    </template>
+  </VTooltip>
+</template>
