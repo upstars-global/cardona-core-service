cc0be962466c80afb60e4efa4b5272bc6827b4b1
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-10-01 10:22:49 +0300

    BAC-3401: add test on ObjectToRowsView

---
+++ tests/unit/ViewGenerator/ObjectToRowsView.spec.ts
@@ -0,0 +1,37 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import ObjectToRowsView from '../../../src/components/templates/ViewGenerator/_components/ObjectToRowsView.vue'
+import { setMountComponent } from '../utils'
+import {testOn} from "../templates/shared-tests/test-case-generator";
+
+const getMountCommentView = setMountComponent(ObjectToRowsView)
+
+
+describe('Component.vue', () => {
+  it('renders item values correctly', () => {
+    const mockItem = {
+      value: {
+        name: 'John Doe',
+        age: 30,
+        job: 'Developer',
+      },
+    }
+
+
+    const wrapper = getMountCommentView({ item: mockItem })
+
+    testOn.existTextValue({ wrapper }, mockItem.value.name)
+    testOn.existTextValue({ wrapper }, mockItem.value.age)
+    testOn.existTextValue({ wrapper }, mockItem.value.job)
+  })
+
+  it('renders empty item correctly', () => {
+    const mockItem = {
+      value: {},
+    }
+
+    const wrapper = getMountCommentView({ item: mockItem })
+
+    expect(wrapper.text()).toBe('')
+  })
+})
