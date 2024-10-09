0dfa7126ed6c7cd10449183a34e594b9bb133719
Vitalii <pecheritsa.vv@gmail.com>
2024-10-09 12:36:15 +0300

    Bac 3401 view generator test (#62)
    * BAC-3401: add base tests
    
    * BAC-3401: reset changes after commit LinkView
    
    * BAC-3401: add test on LocaleView
    
    * BAC-3401: add test on ObjectToRowsView
    
    * BAC-3401: fix lint ObjectToRowsView
    
    * BAC-3401: refactor test LocalView
    
    * BAC-3401: refactor test ObjectToRowsView.spec.ts
    
    * BAC-3401: add test on RegionView.vue
    
    * BAC-3401: add test on TextView.vue
    
    * BAC-3401: add test on LinkView.vue
    
    * BAC-3401: refactor LinkView.vue and add set global params on init wrapper
    
    * BAC-3401: add refactor of LinkView with using testOn utils
    
    * BAC-3401: add refactor of LinkView with using testOn utils
    
    * BAC-3401: remove excess import
    
    * BAC-3401: fix import
    
    * BAC-3401: fix check on empty

--- src/components/templates/ViewGenerator/_components/LinkView.vue
+++ src/components/templates/ViewGenerator/_components/LinkView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed, inject } from 'vue'
-import store from '../../../../store'
+import { useStore } from 'vuex'
 import type { ViewInfo } from '../../../../@model/view'
 
 const props = defineProps<{
@@ -8,6 +8,8 @@ const props = defineProps<{
 }>()
 
 const modal = inject('modal')
+
+const store = useStore()
 
 const canUpdate = computed<boolean>(() =>
   props.item.permission ? store.getters.abilityCan(props.item.permission, 'update') : true,
@@ -29,12 +31,14 @@ const openModal = () => {
     :to="item.value.route"
     :disabled="!canUpdate"
     class="text-primary"
+    data-test-id="link"
     :class="{ 'text-muted': !canUpdate }"
   >
     {{ item.value.title }}
   </RouterLink>
   <p
     v-else
+    data-test-id="title"
     class="text-primary mb-0 cursor-pointer"
     :class="{ 'text-muted': !canUpdate }"
     @click="openModal"
