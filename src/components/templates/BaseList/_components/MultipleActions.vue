21d2546419b53bbd8a38bbe50caaa85d18f22ea5
Konstantin Khomenko <kostya5599@gmail.com>
2024-10-14 18:04:59 +0300

    change padding

--- src/components/templates/BaseList/_components/MultipleActions.vue
+++ src/components/templates/BaseList/_components/MultipleActions.vue
@@ -18,7 +18,7 @@ const emits = defineEmits<Emits>()
 </script>
 
 <template>
-  <div class="table-settings w-100 align-center justify-space-between px-4 py-3">
+  <div class="table-settings w-100 align-center justify-space-between pa-4">
     <span>
       {{ $t('common.numberOfSelected', { number: numberSelectedItems }) }}
     </span>
