fd3c947df5b4a89314f7cb3a0ed63cdc109c1a4e
Vitalii <pecheritsa.vv@gmail.com>
2024-10-16 12:12:27 +0300

    BAC_3403-test checks fields (#77)
    * BAC-3403: add base test of CheckField
    
    * BAC-3403: add refactored CheckField and tests for FieldGroupChecked

---
+++ tests/unit/templates/FieldGenerator/CheckField.spec.ts
@@ -0,0 +1,55 @@
+import { describe, it } from 'vitest'
+import CheckField from '../../../../src/components/templates/FieldGenerator/_components/CheckField.vue'
+import { CheckGroupBaseField } from '../../../../src/@model/templates/baseField'
+import { setMountComponent } from '../../utils'
+import { testOn } from '../shared-tests/test-case-generator'
+import {
+  getCheckBoxElement,
+  isActiveDisabledState, testEmitData,
+  testOnValidLabel,
+} from '../shared-tests/checkbox-field'
+
+const getMountCheckField = setMountComponent(CheckField)
+
+describe('CheckField.vue', () => {
+  const field = new CheckGroupBaseField({
+    label: 'Test Checkbox',
+    value: false,
+    key: 'test-check',
+  })
+
+  const defaultProps = {
+    modelValue: false,
+    disabled: false,
+    field,
+  }
+
+  it('Renders the checkbox with the correct label', () => {
+    const wrapper = getMountCheckField(defaultProps)
+
+    testOnValidLabel(wrapper, field.label)
+  })
+  it('Binds modelValue to checkbox and emits update on change', async () => {
+    const wrapper = getMountCheckField(defaultProps)
+
+    testEmitData(wrapper, [true])
+  })
+
+  it('Exist is disabled state', async () => {
+    const wrapper = getMountCheckField({ ...defaultProps, disabled: true })
+
+    const checkbox = getCheckBoxElement(wrapper)
+
+    isActiveDisabledState(checkbox)
+  })
+
+  it('Updates modelValue correctly when the prop changes', async () => {
+    const wrapper = getMountCheckField(defaultProps)
+
+    const checkbox = getCheckBoxElement(wrapper)
+
+    testOn.isEqual({ wrapper: checkbox.element.checked }, false)
+    await wrapper.setProps({ modelValue: true })
+    testOn.isEqual({ wrapper: checkbox.element.checked }, true)
+  })
+})
