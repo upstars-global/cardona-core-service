77238e8c1ed1b05f2e07b1e46ea79820807ea93f
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-10-27 22:47:05 +0200

    BAC-3409: add first step optimaze code of test

--- tests/unit/templates/FieldGenerator/DateField.spec.ts
+++ tests/unit/templates/FieldGenerator/DateField.spec.ts
@@ -11,6 +11,8 @@ import 'flatpickr/dist/flatpickr.css'
 import { getSelectorTestId } from '../../utils'
 
 const getArialLabelOfCalendar = (date: string) => `[aria-label="${moment(date).format('MMMM D, YYYY')}"]`
+const getInput = (wrapper: VueWrapper, testId: string) => wrapper.find(getSelectorTestId(testId)).find(getSelectorTestId('flat-picker'))
+const getInputsRange = (wrapper: VueWrapper) => ({ from: getInput(wrapper, 'from'), to: getInput(wrapper, 'to') })
 
 vi.mock('vue-i18n', () => ({
   useI18n: () => ({
@@ -34,31 +36,54 @@ const defaultProps = {
     withInitFullData: false,
     isStartDateNow: false,
     isFilter: false,
-    config: {
-      static: false,
-    },
+    config: { static: false },
     allowFutureDate: true,
   },
   errors: false,
 }
 
-const getOverwritedParams = params => ({
-  ...defaultProps, field: { ...defaultProps.field, ...params },
+const getOverwritedParams = (params: Partial<typeof defaultProps.field>) => ({
+  ...defaultProps,
+  field: { ...defaultProps.field, ...params },
 })
+
+const checkEmittedValue = (wrapper: VueWrapper, expectedValue: string, emitIndex = 0) => {
+  expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+  expect(wrapper.emitted()['update:modelValue'][emitIndex][0]).toEqual(expectedValue)
+}
+
+const mountDateFieldComponent = (propsOverride: Partial<typeof defaultProps> = {}) =>
+  mount(DateFieldComponent, {
+    props: { ...defaultProps, ...propsOverride },
+    global: { plugins: [router] },
+  })
+
+const testChangeInputValue = async ({ valueOfSet = '', inputKey, dateRange, indexEmit = 0, isFilter }: OnChangeValueRangeConfig, expectedValue: string) => {
+  const props = getOverwritedParams({ isRangeMode: true, isFilter, config: { static: true } })
+
+  props.modelValue = `${dateRange.from} to ${dateRange.to}`
+
+  const wrapper = mountDateFieldComponent(props)
+  const inputs = getInputsRange(wrapper)
+
+  expect(inputs.from.element.value).toBe(dateRange.from)
+  expect(inputs.to.element.value).toBe(dateRange.to)
+
+  await inputs[inputKey].setValue(valueOfSet)
+  checkEmittedValue(wrapper, expectedValue, indexEmit)
+}
 
 const testOnCallEventEmmitAndEqualValue = (wrapper: VueWrapper, value: string, emmitIndex = 0) => {
   expect(wrapper.emitted()).toHaveProperty('update:modelValue')
   expect(wrapper.emitted()['update:modelValue'][emmitIndex][0]).toEqual(value)
 }
 
-const getInput = (wrapper: VueWrapper, testId: string) => wrapper
-  .find(getSelectorTestId(testId))
-  .find(getSelectorTestId('flat-picker'))
 
-const getInputsRange = (wrapper: VueWrapper) => ({
-  from: getInput(wrapper, 'from'),
-  to: getInput(wrapper, 'to'),
-})
+const setAndCheckInputValue = async (input: DOMWrapper<HTMLInputElement>, initialValue: number, increment: number) => {
+  input.element.value = (initialValue + increment).toString()
+  await input.trigger('input')
+  expect(Number.parseInt(input.element.value, 10)).toBe(initialValue + increment)
+}
 
 interface OnChangeValueRangeConfig {
   inputKey: 'from' | 'to'
@@ -68,93 +93,28 @@ interface OnChangeValueRangeConfig {
   indexEmit?: number
 }
 
-const testOnChangeInputValue = async ({ valueOfSet = '', inputKey, dateRange, indexEmit = 0, isFilter }: OnChangeValueRangeConfig, expectedValue: string) => {
-  const props = getOverwritedParams({ isRangeMode: true, isFilter, config: { static: true } })
-
-  props.modelValue = `${dateRange.from} to ${dateRange.to}`
-
-  const wrapper = mount(DateFieldComponent, {
-    props,
-    global: {
-      plugins: [router],
-    },
-  })
-
-  const inputs = getInputsRange(wrapper)
-
-  expect(inputs.from.element.value).toBe(dateRange.from)
-  expect(inputs.to.element.value).toBe(dateRange.to)
-
-  await inputs[inputKey].setValue(valueOfSet)
-
-  testOnCallEventEmmitAndEqualValue(wrapper, expectedValue, indexEmit)
-}
-
 describe('DateFieldComponent.vue', () => {
-  it('Render picker by range flag', async () => {
-    const wrapper = mount(DateFieldComponent, {
-      props: { ...defaultProps, field: { ...defaultProps.field, isRangeMode: true } },
-      global: {
-        plugins: [router],
-      },
-    })
-
+  it('renders picker by range flag', async () => {
+    const wrapper = mountDateFieldComponent({ field: { ...defaultProps.field, isRangeMode: true } })
     const pickerDataTestIds = ['from', 'to']
 
-    pickerDataTestIds.forEach((testId: string) => {
-      testOn.existElement({ wrapper, testId })
-    })
+    pickerDataTestIds.forEach(testId => testOn.existElement({ wrapper, testId }))
 
     await wrapper.setProps(defaultProps)
-
     await nextTick()
 
-    pickerDataTestIds.forEach((testId: string) => {
-      testOn.notExistElement({ wrapper, testId })
-    })
-
+    pickerDataTestIds.forEach(testId => testOn.notExistElement({ wrapper, testId }))
     testOn.existElement({ wrapper, testId: 'single-picker' })
   })
 
-  it('On call emit on update date value ', async () => {
+  it('emits on date value update', async () => {
     const updatedValue = '2024-11-11T11:00:00.000Z'
-
-    const wrapper = mount(DateFieldComponent, {
-      props: defaultProps,
-      global: {
-        plugins: [router],
-      },
-    })
+    const wrapper = mountDateFieldComponent()
 
     await wrapper.setValue(updatedValue)
-
     await nextTick()
 
-    expect(wrapper.emitted()).toHaveProperty('update:modelValue')
-    expect(wrapper.emitted()['update:modelValue']).toEqual([[updatedValue]])
-  })
-
-  it('Change value on click button day on calendar ', async () => {
-    const updatedValue = '2024-11-10T11:00:00.000Z'
-    const datePickerButton = '2024-11-11'
-    const props = getOverwritedParams({ config: { static: true } })
-
-    props.modelValue = updatedValue
-
-    const wrapper = mount(DateFieldComponent, {
-      props,
-      global: {
-        plugins: [router],
-      },
-    })
-
-    const dayOfCalendar = wrapper.find(getArialLabelOfCalendar(datePickerButton))
-
-    await dayOfCalendar.trigger('click')
-
-    await nextTick()
-
-    testOnCallEventEmmitAndEqualValue(wrapper, `${datePickerButton}T11:00:00.000Z`)
+    checkEmittedValue(wrapper, updatedValue)
   })
 
   it('Change value on click button month on calendar ', async () => {
@@ -184,16 +144,38 @@ describe('DateFieldComponent.vue', () => {
 
     testOnCallEventEmmitAndEqualValue(wrapper, `${datePickerButton}T12:00:00.000Z`)
   })
+  it('Change value on click button day on calendar ', async () => {
+    const updatedValue = '2024-11-10T11:00:00.000Z'
+    const datePickerButton = '2024-11-11'
+    const props = getOverwritedParams({ config: { static: true } })
+
+    props.modelValue = updatedValue
+
+    const wrapper = mount(DateFieldComponent, {
+      props,
+      global: {
+        plugins: [router],
+      },
+    })
+
+    const dayOfCalendar = wrapper.find(getArialLabelOfCalendar(datePickerButton))
+
+    await dayOfCalendar.trigger('click')
+
+    await nextTick()
+
+    testOnCallEventEmmitAndEqualValue(wrapper, `${datePickerButton}T11:00:00.000Z`)
+  })
 
   it('Change value on change input oh hours ', async () => {
     const updatedValue = '2024-10-10T11:00:00.000Z'
 
-    const props2 = getOverwritedParams({ config: { static: true } })
+    const props = getOverwritedParams({ config: { static: true } })
 
-    props2.modelValue = updatedValue
+    props.modelValue = updatedValue
 
     const wrapper = mount(DateFieldComponent, {
-      props: props2,
+      props,
       global: {
         plugins: [router],
       },
@@ -208,96 +190,42 @@ describe('DateFieldComponent.vue', () => {
     const initialHour = Number.parseInt(hourInput.element.value, 10)
     const initialMinute = Number.parseInt(minuteInput.element.value, 10)
 
-    hourInput.element.value = (initialHour + 1).toString()
-    await hourInput.trigger('input')
-
-    expect(Number.parseInt(hourInput.element.value, 10)).toBe(initialHour + 1)
-
-    minuteInput.element.value = (initialMinute + 1).toString()
-    await minuteInput.trigger('input')
-
-    expect(Number.parseInt(minuteInput.element.value, 10)).toBe(initialMinute + 1)
+    await setAndCheckInputValue(hourInput, initialHour, 1)
+    await setAndCheckInputValue(minuteInput, initialMinute, 1)
   })
 
-  it('Filter mode on change input value From with filled value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'from',
-      isFilter: true,
-      dateRange: {
-        from: '2024-11-11T11:00:00.000Z',
-        to: '2024-11-12T11:00:00.000Z',
+  it('updates in filter mode with empty from value', async () => {
+    await testChangeInputValue(
+      {
+        inputKey: 'from',
+        isFilter: true,
+        dateRange: { from: '', to: '' },
+        valueOfSet: '2024-11-12T11:00:00.000Z',
       },
-    }
-
-    await testOnChangeInputValue(config, `${moment(1432252800).format()} to ${config.dateRange.to}`)
+      `2024-11-12T11:00:00.000Z to ${moment().format()}`,
+    )
   })
 
-  it('Filter mode on change input value To with filled value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'to',
-      isFilter: true,
-      dateRange: {
-        from: '2024-11-11T11:00:00.000Z',
-        to: '2024-11-12T11:00:00.000Z',
+  it('updates in filter mode with filled from value', async () => {
+    await testChangeInputValue(
+      {
+        inputKey: 'from',
+        isFilter: true,
+        dateRange: { from: '2024-11-11T11:00:00.000Z', to: '2024-11-12T11:00:00.000Z' },
       },
-      indexEmit: 1,
-    }
-
-    await testOnChangeInputValue(config, `${config.dateRange.from} to ${moment().format()}`)
+      `${moment(1432252800).format()} to 2024-11-12T11:00:00.000Z`,
+    )
   })
 
-  it('Filter mode on change input value From with initial empty value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'from',
-      isFilter: true,
-      valueOfSet: '2024-11-12T11:00:00.000Z',
-      dateRange: {
-        from: '',
-        to: '',
+  it('updates in non-filter mode with empty to value', async () => {
+    await testChangeInputValue(
+      {
+        inputKey: 'to',
+        isFilter: false,
+        dateRange: { from: '', to: '' },
+        valueOfSet: '2024-11-12T11:00:00.000Z',
       },
-    }
-
-    await testOnChangeInputValue(config, `2024-11-12T11:00:00.000Z to ${moment().format()}`)
-  })
-
-  it('Filter mode on change input value To with initial empty value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'to',
-      isFilter: true,
-      valueOfSet: '2024-11-12T11:00:00.000Z',
-      dateRange: {
-        from: '',
-        to: '',
-      },
-    }
-
-    await testOnChangeInputValue(config, '1970-01-17T16:50:52+03:00 to 2024-11-12T11:00:00.000Z')
-  })
-
-  it('Filter mode on change input value To with initial empty value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'to',
-      isFilter: false,
-      valueOfSet: '2024-11-12T11:00:00.000Z',
-      dateRange: {
-        from: '',
-        to: '',
-      },
-    }
-
-    await testOnChangeInputValue(config, ' to 2024-11-12T11:00:00.000Z')
-  })
-  it('Filter mode on change input value To with initial empty value', async () => {
-    const config: OnChangeValueRangeConfig = {
-      inputKey: 'from',
-      isFilter: false,
-      valueOfSet: '2024-11-12T11:00:00.000Z',
-      dateRange: {
-        from: '',
-        to: '',
-      },
-    }
-
-    await testOnChangeInputValue(config, '2024-11-12T11:00:00.000Z to ')
+      ' to 2024-11-12T11:00:00.000Z',
+    )
   })
 })
