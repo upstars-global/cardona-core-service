bd9b39b9b1121d33e16cbf004f2f5ad7c2c1b7d7
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-10-29 19:45:37 +0200

    BAC-3409: base test for TimeField

--- tests/unit/templates/shared-tests/general.ts
+++ tests/unit/templates/shared-tests/general.ts
@@ -1,9 +1,14 @@
 import type { VueWrapper } from '@vue/test-utils'
 import { ExpectMethods, WrapperProperties, testCaseGenerator } from './test-case-generator'
+import {expect} from "vitest";
 
 export const isCalledEmitEvent = (wrapper: VueWrapper, actionEmit: string) => {
   testCaseGenerator({
     methodExpect: ExpectMethods.ToBeTruthy,
     property: { name: WrapperProperties.Emitted, value: actionEmit },
   })({ wrapper })
+}
+
+export const expectedEmitValue = (wrapper: VueWrapper, value: string | number, indexEmit?: number = 0) => {
+  expect(wrapper.emitted()['update:modelValue'][indexEmit][0]).toEqual(value)
 }
