829cdb2a302ce8acc57fad64b24a3e8dc30b79e5
Artem Oleinychenko <a.oleinychenko@upstars.com>
2024-11-13 12:43:08 +0200

    BAC-3981 do not update item if canUpdateCb false

--- src/components/templates/BaseList/index.vue
+++ src/components/templates/BaseList/index.vue
@@ -324,7 +324,7 @@ onChangePagination(() => {
 const checkSlotExistence = (slotName: string): boolean => !!slots[slotName]
 
 const getUpdateRoute = ({ id }): Location => {
-  return isExistsUpdatePage && (canUpdate || canUpdateSeo)
+  return isExistsUpdatePage && (canUpdateCb?.() ?? true) && (canUpdate || canUpdateSeo)
     ? { name: UpdatePageName, params: { id } }
     : {}
 }
