cb5f57f59c51acc24e55185744bd73f4a44e8d40
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-11-21 00:12:52 +0200

    BAC-3423: save base test case in comment for next day

--- tests/unit/components/templates/BaseList/_components/ListSearch.spec.ts
+++ tests/unit/components/templates/BaseList/_components/ListSearch.spec.ts
@@ -49,4 +49,42 @@ describe('ListSearch.vue', () => {
     /// Check on not rendering search input
     testOn.notExistElement({ wrapper, testId: 'search-input' })
   })
+
+  it('Check on render content of slots', async () => {
+    const global = {}
+
+    const leftSearchSlotText = 'Left search btn'
+    const rightSearchSlotText = 'Right search btn'
+
+    const testCaseParamsForSlots = [
+      {
+        key: 'left',
+        text: leftSearchSlotText,
+      }, {
+        key: 'right',
+        text: rightSearchSlotText,
+      },
+    ]
+
+    const slots = {
+      'left-search-btn': `<div data-test-id="left-search-btn"> ${leftSearchSlotText} </div>`,
+      'right-search-btn': `<div data-test-id="right-search-btn"> ${rightSearchSlotText} </div>`,
+    }
+
+    const wrapper = getMountListSearch(props, global, slots)
+
+    testCaseParamsForSlots.forEach(param => {
+      const testId = `${param.key}-search-btn`
+
+      testOn.existElement({ wrapper, testId })
+      testOn.existTextValue({ wrapper, testId }, param.text)
+    })
+  })
+
+  /// TODO test cases
+  /// Tender and call action on default slot content right-search-btn
+  /// Check on exist export format selector and call event export-format-selected
+  /// Render content on config.filterList?.isNotEmpty like sepa
+  // rate test on render content  (isOpenFilterBlock, config?.small)
+  /// Call event on update value of v-model="searchQuery"
 })
