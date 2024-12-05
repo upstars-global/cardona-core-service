f6c01462c1ce49b0a24a09e05a70404e153b2309
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-12-05 11:02:21 +0200

    BAC-3417: add valid first test with base props

--- tests/unit/components/templates/CTable.spec.ts
+++ tests/unit/components/templates/CTable.spec.ts
@@ -4,6 +4,7 @@ import { setMountComponent } from '../../utils'
 import { ListFieldType, TableField } from '../../../../src/@model/templates/tableFields'
 import type { SelectMode } from '../../../../src/@model/enums/selectMode'
 import type { SortItem } from '../../../../src/@core/types'
+import { testOn } from '../../templates/shared-tests/test-case-generator'
 
 const getMountCTable = setMountComponent(CTable)
 
@@ -44,6 +45,12 @@ const defaultProps: Props = {
   selectedItems: [],
 }
 
+const global = {}
+
+const slots = {
+  [`cell(${rows[0].key})`]: `<div class="default-slot-content" data-test-id="slot-${rows[0].key}">Content</div>`,
+}
+
 // const slots = {
 //   'modal-header': '<div class="modal-header-slot">Custom Modal Header</div>',
 //   'default': '<div class="default-slot-content">Custom Modal Content</div>',
@@ -51,7 +58,9 @@ const defaultProps: Props = {
 
 describe('CTable', () => {
   it('Render default state of component', async () => {
-    const wrapper = getMountCTable(defaultProps)
+    const wrapper = getMountCTable(defaultProps, global, slots)
+
+    testOn.equalTextValue({ wrapper, testId: `slot-${rows[0].key}` }, 'Content')
 
     console.log(wrapper.html())
   })
