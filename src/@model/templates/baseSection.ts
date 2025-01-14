95db8496609530118f538c8860f98ea9fe853235
Konstantin Khomenko <kostya5599@gmail.com>
2025-01-14 20:39:51 +0200

    model

--- src/@model/templates/baseSection.ts
+++ src/@model/templates/baseSection.ts
@@ -1,8 +1,12 @@
+import type { IValidationError } from '../../services/api/config'
+
 export enum PageType {
   Create = 'create',
   Update = 'update',
   Empty = '',
 }
+
+export type IValidationErrorCb = (entity: string, payload: IValidationError) => { localeKey: string; fieldKey?: string; toastOptions?: object }
 
 export interface UseEntityType<T> {
   readonly entityName: string
@@ -10,5 +14,6 @@ export interface UseEntityType<T> {
   readonly EntityFormClass: { new (data?: any): T }
   readonly onSubmitCallback?: (id: string) => void
   readonly onBeforeSubmitCb?: (formData: any) => boolean
+  readonly validationErrorCb?: IValidationErrorCb
 }
 export default {}
