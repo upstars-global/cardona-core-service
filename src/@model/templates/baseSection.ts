a2fcc8a893330d09a3c0315091af82d6cebe16da
Konstantin Khomenko <57718292+KonstantinKhomenko@users.noreply.github.com>
2025-02-06 15:21:20 +0200

    BAC-2230_fix-rates-validation-core (#176)
    * params type changed
    
    * currency id
    
    * model
    
    * added cb
    
    * api service
    
    * BAC-2230: modern test by updated component
    
    ---------
    
    Co-authored-by: v.pecheritsya <pecheritsa.vv@gmail.com>

--- src/@model/templates/baseSection.ts
+++ src/@model/templates/baseSection.ts
@@ -1,8 +1,12 @@
+import type { IValidationError } from '../../services/api/config'
+
 export enum PageType {
   Create = 'create',
   Update = 'update',
   Empty = '',
 }
+
+export type IValidationErrorCb = (entity: string, payload: IValidationError) => { localeKey: string; fieldKey?: string; toastOptions?: object }
 
 export interface UseEntityType<T> {
   readonly entityName: string
@@ -11,5 +15,6 @@ export interface UseEntityType<T> {
   readonly onSubmitCallback?: (id: string) => void
   readonly onBeforeSubmitCb?: (formData: any) => boolean
   readonly onSerializeFormCb?: (transformedFormData: unknown, rawFormData: Ref<unknown>) => unknown
+  readonly validationErrorCb?: IValidationErrorCb
 }
 export default {}
