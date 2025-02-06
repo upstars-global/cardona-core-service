0b84ee2e13fb833a03d5473ee3b870e45f3c04c7
Artem Oleinychenko <a.oleinychenko@upstars.com>
2025-02-06 14:24:04 +0200

    BAC-4166: return 404 page

---
+++ src/pages/error/404.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import ErrorHeader from '../../components/ErrorHeader.vue'
+
+defineOptions({
+  name: 'NotFound',
+})
+</script>
+
+<template>
+  <div class="grid-container">
+    <div class="misc-wrapper d-flex align-center justify-center">
+      <ErrorHeader
+        :status-code="$t('page.404.statusCode')"
+        :title="$t('page.404.title')"
+        :description="$t('page.404.description')"
+      />
+    </div>
+    <div class="d-flex align-center justify-center my-2">
+      <VBtn
+        to="/"
+        class="mb-12"
+      >
+        {{ $t('page.404.backToHome') }}
+      </VBtn>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "../../@core/scss/template/pages/misc.scss";
+.grid-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  border-radius: 8px;
+}
+</style>
