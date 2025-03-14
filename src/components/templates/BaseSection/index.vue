577ba9d8828231961286aad2ce61db642678a23d
Artem Oleinychenko <a.oleinychenko@upstars.com>
2025-03-14 14:31:36 +0200

    fix: correct path

--- src/components/templates/BaseSection/index.vue
+++ src/components/templates/BaseSection/index.vue
@@ -13,9 +13,9 @@ import RemoveModal from '../../../components/BaseModal/RemoveModal.vue'
 import ConfirmModal from '../../../components/BaseModal/ConfirmModal.vue'
 import { ModalsId } from '../../..//@model/modalsId'
 import { useRedirectToNotFoundPage } from '../../../helpers/router'
+import { setTabError } from '../../../components/templates/BaseSection/composables/tabs'
+import { generateEntityUrl } from '../../../components/templates/BaseSection/composables/entity'
 import BaseSectionLoading from './BaseSectionLoading.vue'
-import { setTabError } from '@/components/templates/BaseSection/composables/tabs'
-import { generateEntityUrl } from '@/components/templates/BaseSection/composables/entity'
 
 const props = withDefaults(defineProps<{
   withReadAction?: boolean
