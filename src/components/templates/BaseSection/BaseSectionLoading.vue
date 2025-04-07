af0a849b063cfbb6035a40c6b359d6adc4ee5cdb
v.pecheritsya <pecheritsa.vv@gmail.com>
2025-04-07 17:51:26 +0300

    BAC-4266: add centering spinner and remove scroll

--- src/components/templates/BaseSection/BaseSectionLoading.vue
+++ src/components/templates/BaseSection/BaseSectionLoading.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { nextTick, ref, watch } from 'vue'
 import { VColors } from '../../../@model/vuetify'
 
 interface Props {
@@ -11,32 +12,93 @@ const props = withDefaults(defineProps<Props>(), {
   opacity: 0.5,
   loader: true,
 })
+
+const overlayWrapper = ref<HTMLElement | null>(null)
+const spinnerY = ref(0)
+
+const updateSpinnerPosition = () => {
+  const scrollY = window.scrollY || window.pageYOffset
+  const viewportHeight = window.innerHeight
+
+  if (!overlayWrapper.value)
+    return
+
+  const wrapperTop = overlayWrapper.value.getBoundingClientRect().top + scrollY
+  const relativeY = scrollY + viewportHeight / 2 - wrapperTop
+
+  spinnerY.value = relativeY
+}
+
+const scrollTop = ref(0)
+
+const lockScroll = () => {
+  scrollTop.value = window.scrollY
+  document.body.style.position = 'fixed'
+  document.body.style.top = `-${scrollTop.value}px`
+  document.body.style.left = '0'
+  document.body.style.right = '0'
+  document.body.style.width = '100%'
+}
+
+const unlockScroll = () => {
+  document.body.style.position = ''
+  document.body.style.top = ''
+  document.body.style.left = ''
+  document.body.style.right = ''
+  document.body.style.width = ''
+  window.scrollTo(0, scrollTop.value)
+}
+
+watch(() => props.loading, async value => {
+  if (value) {
+    await nextTick()
+    lockScroll()
+    updateSpinnerPosition()
+    window.addEventListener('scroll', updateSpinnerPosition)
+    window.addEventListener('resize', updateSpinnerPosition)
+  }
+  else {
+    unlockScroll()
+    window.removeEventListener('scroll', updateSpinnerPosition)
+    window.removeEventListener('resize', updateSpinnerPosition)
+  }
+})
 </script>
 
 <template>
   <div
+    ref="overlayWrapper"
     class="v-overlay-wrapper"
     style="position: relative;"
   >
     <slot />
 
-    <VOverlay
-      :model-value="props.loading"
-      :opacity="props.opacity"
-      contained
-      persistent
-      scrim="rgba(255, 255, 255, 0.8) !important"
-      class="
-      d-flex
-      align-center
-      justify-center"
+    <div
+      v-if="props.loading"
+      class="custom-overlay"
+      :style="{ backgroundColor: `rgba(255,255,255,${props.opacity})` }"
     >
       <VProgressCircular
         v-if="props.loader"
         size="40"
         indeterminate
         :color="VColors.Primary"
+        class="custom-spinner"
+        :style="{ top: `${spinnerY}px` }"
       />
-    </VOverlay>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.custom-overlay {
+  position: absolute;
+  inset: 0;
+  z-index: 10;
+}
+.custom-spinner {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
