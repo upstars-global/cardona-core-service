06c339b5d90bbd3ce971af78c260a8319df60aff
v.pecheritsya <pecheritsa.vv@gmail.com>
2025-04-07 16:31:23 +0300

    BAC-4266: overwrite base section loading

--- src/components/templates/BaseSection/BaseSectionLoading.vue
+++ src/components/templates/BaseSection/BaseSectionLoading.vue
@@ -1,72 +1,42 @@
 <script setup lang="ts">
 import { VColors } from '../../../@model/vuetify'
 
-defineProps<{ loading: boolean }>()
+interface Props {
+  loading: boolean
+  opacity?: number
+  loader?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  opacity: 0.5,
+  loader: true,
+})
 </script>
 
 <template>
-  <div class="main-section-container">
-    <Transition name="fade">
-      <div
-        v-show="loading"
-        class="loader-overlay"
-      >
-        <VProgressCircular
-          indeterminate
-          size="64"
-          data-test-id="loader"
-          :color="VColors.Primary"
-        />
-      </div>
-    </Transition>
-    <div
-      v-show="!loading"
-      data-test-id="not-active-loader-content"
+  <div
+    class="v-overlay-wrapper"
+    style="position: relative;"
+  >
+    <slot />
+
+    <VOverlay
+      :model-value="props.loading"
+      :opacity="props.opacity"
+      contained
+      persistent
+      scrim="rgba(255, 255, 255, 0.8) !important"
+      class="
+      d-flex
+      align-center
+      justify-center"
     >
-      <slot />
-    </div>
+      <VProgressCircular
+        v-if="props.loader"
+        size="40"
+        indeterminate
+        :color="VColors.Primary"
+      />
+    </VOverlay>
   </div>
 </template>
-
-<style lang="scss">
-.loader-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 90vh;
-  background-color: rgb(var(--v-theme-surface));
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-}
-
-.base-modal .loader-overlay {
-  position: relative;
-  height: auto;
-  margin: 100px auto;
-
-  &.fade-enter-active,
-  &.fade-leave-active {
-    transition: all 0s ease;
-    display: none;
-  }
-}
-</style>
-
-<style scoped lang="scss">
-.main-section-container {
-  position: relative;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
