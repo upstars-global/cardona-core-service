5f7bec308cfb48d435022e27f4ffe7b73185f36b
v.pecheritsya <pecheritsa.vv@gmail.com>
2025-04-28 12:43:57 +0300

    BAC-5186: fix path of component

--- src/components/templates/_components/BtnTooltip.vue
+++ src/components/templates/_components/BtnTooltip.vue
@@ -2,7 +2,7 @@
 import type { TranslateResult } from 'vue-i18n'
 import type { VColors, VSizes, VVariants } from '../../../@model/vuetify'
 import type { IconsList } from '../../../@model/enums/icons'
-import { IS_TEST_ENV } from '@/utils/constants'
+import { IS_TEST_ENV } from '../../../utils/constants'
 
 interface Props {
   tooltipText: TranslateResult
