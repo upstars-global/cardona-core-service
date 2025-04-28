81443892d928ffcc7ef630f5fd0b20f40d5ada7b
v.pecheritsya <pecheritsa.vv@gmail.com>
2025-04-28 12:42:49 +0300

    BAC-5186: add full tests and make coverage for 100%

--- tests/unit/components/templates/BaseList/_components/BtnTooltip.spec.ts
+++ tests/unit/components/templates/BaseList/_components/BtnTooltip.spec.ts
@@ -1,6 +1,6 @@
-import { beforeEach, describe, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import type { TranslateResult } from 'vue-i18n'
-import { getSelectorTestId, setMountComponent } from '../../../../utils'
+import { clickTrigger, setMountComponent } from '../../../../utils'
 import BtnTooltip from '../../../../../../src/components/templates/_components/BtnTooltip.vue'
 import type { VColors, VSizes, VVariants } from '../../../../../../src/@model/vuetify'
 import type { IconsList } from '../../../../../../src/@model/enums/icons'
@@ -20,7 +20,7 @@ interface BtnTooltipProps {
   label?: string
 }
 
-const defaultProps = {
+const defaultProps: BtnTooltipProps = {
   tooltipText: 'Some tooltip text',
   label: 'Some button text',
 }
@@ -32,14 +32,33 @@ beforeEach(() => {
 })
 
 describe('BtnTooltip.vue', () => {
-  it('Renders correctly base elements', async () => {
+  it('Renders correctly content text of component', () => {
     const wrapper = getMountComponent(props)
 
     testOn.existTextValue({ wrapper, testId: 'tooltip-activator' }, props.label)
+    testOn.existTextValue({ wrapper, selector: '.v-overlay__content' }, props.tooltipText)
+  })
 
-    await wrapper.find(getSelectorTestId('tooltip-activator')).trigger('mouseover')
-    await wrapper.trigger('mouseover')
+  it('Check call event "click"', async () => {
+    const wrapper = getMountComponent(props)
 
-    console.log(wrapper.html())
+    await clickTrigger({ wrapper, testId: 'tooltip-activator' })
+
+    testOn.isCalledEmitEvent({ wrapper }, 'click')
+  })
+
+  it('Check render  default slot content ', () => {
+    const SLOT_TEXT = 'Slot content'
+    const wrapper = getMountComponent(props, {}, { default: `<div data-test-id="slot-content">${SLOT_TEXT}</div>` })
+
+    testOn.existTextValue({ wrapper, testId: 'slot-content' }, SLOT_TEXT)
+  })
+
+  it('Check not calling event "click" on disabled state', async () => {
+    const wrapper = getMountComponent({ ...props, disabled: true })
+
+    await clickTrigger({ wrapper, testId: 'tooltip-activator' })
+
+    expect(wrapper.emitted()).toStrictEqual({})
   })
 })
