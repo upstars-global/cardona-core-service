96bbb66bcaaf88f52d1e1ddcc062bd6496d1f299
SimpllleUser <pecheritsa.vv@gmail.com>
2025-05-12 14:40:06 +0300

    BAC-5156: add modal footer

---
+++ src/components/BaseModal/ModalFooter.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { VColors, VVariants } from '../../@model/vuetify'
+
+interface ButtonConfig {
+  label?: string
+  variant?: VVariants
+  color?: VColors
+  disabled?: boolean
+}
+
+interface Props {
+  accept: ButtonConfig
+  cancel: ButtonConfig
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  accept: () => ({
+    color: VColors.Primary,
+    disabled: false,
+  }),
+  cancel: () => ({
+    label: '',
+    color: VColors.Secondary,
+    variant: VVariants.Outlined,
+    disabled: false,
+  }),
+})
+
+defineEmits<Emits>()
+
+enum Action {
+  Accept = 'accept',
+  Cancel = 'cancel',
+}
+
+interface Emits {
+  (event: Action.Accept): void
+  (event: Action.Cancel): void
+}
+
+const defaultAcceptProps = {
+  label: '',
+  color: VColors.Primary,
+  disabled: false,
+}
+
+const defaultCancelProps = {
+  label: '',
+  color: VColors.Secondary,
+  variant: VVariants.Outlined,
+  disabled: false,
+}
+
+const acceptButtonConfig = computed(() => ({
+  ...defaultAcceptProps,
+  ...props.accept,
+}))
+
+const cancelButtonConfig = computed(() => ({
+  ...defaultCancelProps,
+  ...props.accept,
+}))
+</script>
+
+<template>
+  <hr class="my-6">
+  <div class="px-6 pb-4 d-flex justify-end items-center">
+    <VBtn
+      v-bind="cancelButtonConfig"
+      @click="$emit(Action.Cancel)"
+    >
+      {{ cancelButtonConfig?.label }}
+    </VBtn>
+    <VBtn
+      class="ml-4"
+      v-bind="acceptButtonConfig"
+      @click="$emit(Action.Accept)"
+    >
+      {{ acceptButtonConfig?.label }}
+    </VBtn>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
