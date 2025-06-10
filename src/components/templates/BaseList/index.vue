060e12528045c58e2d30b19e9d2feacf20a7a82b
k-khomenko-upstars <k.khomenko@upstars.com>
2025-06-10 21:27:59 +0300

    added slot props

--- src/components/templates/BaseList/index.vue
+++ src/components/templates/BaseList/index.vue
@@ -685,7 +685,10 @@ defineExpose({ reFetchList, resetSelectedItem, selectedItems, disableRowIds, sor
 
         <!-- Table field settings -->
         <div class="d-flex align-center justify-end">
-          <slot :name="BaseListSlots.TableFieldSetting" />
+          <slot
+            :name="BaseListSlots.TableFieldSetting"
+            :selected-items="selectedItems"
+          />
 
           <TableFields
             v-model="selectedFields"
