a44fa3fd59e1409e94ef2e4c139b6e7bcab56c82
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2025-06-17 15:39:21 +0300

    BAC-5573: refactor update default SEO field value in SeoForm (#291)
    Co-authored-by: Vitalii <pecheritsa.vv@gmail.com>

--- src/@model/seo.ts
+++ src/@model/seo.ts
@@ -7,7 +7,7 @@ export interface SeoData {
   readonly description?: string
 }
 
-const DEFAULT_META_TITLE = '&nbsp;'
+const DEFAULT_SEO_FIELD_VALUE = '&nbsp;'
 
 export class SeoForm {
   readonly metaTitle: TextBaseField
@@ -17,20 +17,20 @@ export class SeoForm {
   constructor(data?: SeoData) {
     this.metaTitle = new TextBaseField({
       key: 'metaTitle',
-      value: data?.metaTitle || DEFAULT_META_TITLE,
+      value: data?.metaTitle || DEFAULT_SEO_FIELD_VALUE,
       label: i18n.t('seo.metaTitle'),
       isLocalization: true,
     })
     this.metaDescription = new TextareaBaseField({
       key: 'metaDescription',
-      value: data?.metaDescription,
+      value: data?.metaDescription || DEFAULT_SEO_FIELD_VALUE,
       label: i18n.t('seo.metaDescription'),
       placeholder: i18n.t('seo.metaDescription'),
       isLocalization: true,
     })
     this.description = new RichTextBaseField({
       key: 'description',
-      value: data?.description,
+      value: data?.description || DEFAULT_SEO_FIELD_VALUE,
       label: i18n.t('seo.description'),
       form: null,
       isLocalization: true,
