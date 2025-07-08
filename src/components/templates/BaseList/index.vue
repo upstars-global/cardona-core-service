b176876c7fa64274e751d2e3ba38b1be332231f7
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2025-07-08 13:37:14 +0300

    BAC-5668: add pagination in top (#300)
    * BAC-5668: add pagination in top
    
    * BAC-5668: add fix
    
    ---------
    
    Co-authored-by: Vitalii <pecheritsa.vv@gmail.com>

--- src/components/templates/BaseList/index.vue
+++ src/components/templates/BaseList/index.vue
@@ -651,7 +651,21 @@ defineExpose({ reFetchList, resetSelectedItem, selectedItems, disableRowIds, sor
     />
 
     <slot :name="BaseListSlots.CustomFilter" />
-
+    <div
+      v-if="config.withTopPagination && items.isNotEmpty && config.pagination"
+      class="mb-6"
+      :class="{ 'mt-6': config.filterList?.isEmpty && config.hideSearchBlock }"
+    >
+      <ListPagination
+        data-test-id="list-pagination"
+        :model-value="currentPage"
+        :link-gen="linkGenerator"
+        :pagination-config="paginationConfig"
+        :data-meta="dataMeta"
+        :small="config.small"
+        @update:model-value="setPage"
+      />
+    </div>
     <VCard class="table-card-settings">
       <div
         v-if="config.withSettings && (!canUpdate || selectedItems.isEmpty)"
@@ -684,7 +698,6 @@ defineExpose({ reFetchList, resetSelectedItem, selectedItems, disableRowIds, sor
             </template>
           </VueSelect>
         </div>
-
         <!-- Table field settings -->
         <div class="d-flex align-center justify-end">
           <slot
