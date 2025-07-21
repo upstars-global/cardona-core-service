e677c67074ee2074607b80f5c4a0d67d12c63877
Vitalii <pecheritsa.vv@gmail.com>
2025-07-21 17:16:46 +0300

    BAC-5733: remove getters from stores and use actual userStore pinia

--- src/store/modules/tags.ts
+++ src/store/modules/tags.ts
@@ -1,16 +1,19 @@
 import ApiService from '../../services/api'
 import type { OptionsItem } from '../../@model'
 import { ListData } from '../../@model'
+import { useUserStore } from '../../stores/user'
 
 export default {
   namespaced: true,
   actions: {
-    async fetchTags({ rootGetters }) {
+    async fetchTags() {
+      const userStore = useUserStore()
+
       return new ListData<OptionsItem>(
         await ApiService.request({
           type: 'App.V2.Tags.List',
           filter: {
-            project: rootGetters.selectedProject.alias,
+            project: userStore.selectedProject.alias,
           },
         }),
       )
