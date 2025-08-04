b36500f6c25b645300e32caf818c293548500c26
k-khomenko-upstars <k.khomenko@upstars.com>
2025-08-04 12:07:48 +0300

    BAC-5667_dashboard-core (#299)
    * chart plugin updated
    
    * chart model
    
    * base chart
    
    * added icon
    
    * layout-rows icon added
    
    * icons added
    
    * active menu model
    
    * active menu component
    
    * import added
    
    * removed deep & imports
    
    * added import
    
    * amount helper
    
    * number directive
    
    * chart config
    
    * custom tooltip
    
    * config changed
    
    * line hover
    
    * edits after code review
    
    * edits after test

---
+++ src/@model/templates/chart.ts
@@ -0,0 +1,137 @@
+import type { ChartOptions, ChartType, ScriptableContext } from 'chart.js'
+import { amountFormatter } from '../../helpers/amount'
+
+export const ChartTypes: Record<string, ChartType> = {
+  Line: 'line',
+  Bar: 'bar',
+} as const
+
+export type AllowedChartType = typeof ChartTypes[keyof typeof ChartTypes]
+
+const ticksCallback = (value: number, isPercent?: boolean): string => isPercent ? `${value}%` : amountFormatter(value)
+
+export const getChartConfig = (options: { chartType: AllowedChartType; isPercent?: boolean }) => {
+  const { chartType, isPercent } = options
+
+  const tooltip = {
+    mode: 'index',
+    intersect: true,
+  }
+
+  const legend = {
+    position: 'bottom',
+    labels: {
+      usePointStyle: true,
+      pointStyle: 'circle',
+      padding: 12,
+    },
+  }
+
+  const xTicks = {
+    autoSkip: false,
+    maxRotation: 0,
+    minRotation: 0,
+    callback(this, _, index: number) {
+      const labels = this.chart.data.labels as string[]
+
+      return labels[index] ? labels[index].split(' ') : ''
+    },
+  }
+
+  const yTicks = {
+    callback: (value: number) => ticksCallback(value, isPercent),
+  }
+
+  const grid = {
+    display: true,
+    color: '#E9E9F0',
+  }
+
+  const border = {
+    dash: [4, 4],
+  }
+
+  const chartsOptions: Record<AllowedChartType, ChartOptions<AllowedChartType>> = {
+    [ChartTypes.Bar]: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend,
+        tooltip,
+      },
+      scales: {
+        x: {
+          stacked: true,
+          ticks: xTicks,
+          grid,
+          border,
+        },
+        y: {
+          stacked: true,
+          ticks: yTicks,
+          grid,
+          border,
+        },
+      },
+      hover: {
+        mode: null,
+      },
+    },
+
+    [ChartTypes.Line]: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend,
+        tooltip,
+      },
+      elements: {
+        line: {
+          tension: 0,
+          borderColor: (ctx: ScriptableContext<typeof ChartTypes.Line>) => ctx.dataset.backgroundColor as string,
+          borderWidth: 3,
+          fill: false,
+          borderCapStyle: 'round',
+          borderJoinStyle: 'round',
+        },
+        point: {
+          radius: 3,
+          hoverRadius: 5,
+        },
+      },
+
+      onHover: (event, activeElements, chart) => {
+        if (activeElements.length) {
+          const dataIndex = activeElements[0].index
+
+          const newActive = chart.data.datasets.map((_, datasetIndex) => ({
+            datasetIndex,
+            index: dataIndex,
+          }))
+
+          chart.setActiveElements(newActive)
+          chart.tooltip.setActiveElements(newActive)
+        }
+        else {
+          chart.setActiveElements([])
+          chart.tooltip.setActiveElements([])
+        }
+      },
+
+      scales: {
+        x: {
+          ticks: xTicks,
+          grid,
+          border,
+        },
+        y: {
+          ticks: yTicks,
+          grid,
+          border,
+        },
+      },
+    },
+  }
+
+  return chartsOptions[chartType]
+}
