4f08b8e32e7808cb8e264fffa0a3c0aa04dd2a2b
danfeleniuk <d.feleniuk@upstars.com>
2025-08-20 17:34:47 +0300

    PEOPWE-304: optional Froala config

--- src/@model/templates/baseField/rich-text.ts
+++ src/@model/templates/baseField/rich-text.ts
@@ -6,15 +6,18 @@ import { BaseField } from './base'
 
 export interface IRichTextBaseField extends IBaseField {
   readonly value?: string
+  readonly config?: Record<string, unknown>
 }
 
 export class RichTextBaseField extends BaseField implements IRichTextBaseField {
   readonly component: Component = markRaw(RichTextField)
   protected _value?: string
+  readonly config?: Record<string, unknown>
 
   constructor(field: IRichTextBaseField) {
     super(field)
     this._value = field.value ?? ''
+    this.config = field.config
   }
 
   transformField() {
