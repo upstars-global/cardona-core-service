972ef7a2f5b295b5c39c7bac8a40373bc7180152
Vitalii <pecheritsa.vv@gmail.com>
2025-09-08 23:39:31 +0300

    Constructor: add Vuetify toolbar with primary color and update page layout

--- src/pages/constructor/index.vue
+++ src/pages/constructor/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { VColors } from '../../@model/vuetify'
+
 defineOptions({
   name: 'Constructor',
 })
@@ -6,6 +8,12 @@ defineOptions({
 
 <template>
   <div>
-    Constructor
+    <VToolbar
+      title="Constructor"
+      :color="VColors.Primary"
+    />
+    <div class="pa-4">
+      body
+    </div>
   </div>
 </template>
