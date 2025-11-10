e5dd43a542135104b307efa2d00f126be5eabbdc
Vitalii <v.pecheritsya@upstars.com>
2025-11-10 20:26:44 +0200

    BAC-6572: Add tooltip to `NotificationExport` button and update i18n for notifications

--- src/components/NotificationExport/index.vue
+++ src/components/NotificationExport/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { IconsList } from '../../@model/enums/icons'
 import { VColors, VSizes, VVariants } from '../../@model/vuetify'
+import { Location } from '../../@model/enums/tooltipPlacement'
 
 defineOptions({
   name: 'NotificationExport',
@@ -9,21 +10,27 @@ defineOptions({
 
 <template>
   <div>
-    <VBtn
-      :variant="VVariants.Text"
-      :size="VSizes.XSmall"
-      rounded="lg"
-      icon
-    >
-      <VBadge
-        :color="VColors.Primary"
-        dot
-      >
-        <VIcon
-          :icon="IconsList.BellIcon"
-          :size="20"
-        />
-      </VBadge>
-    </VBtn>
+    <VTooltip :location="Location.Left" :text="$t('notification._')">
+      <template #activator="{ props }">
+        <VBtn
+          v-bind="props"
+          :variant="VVariants.Text"
+          :size="VSizes.XSmall"
+          :color="VColors.Secondary"
+          rounded="lg"
+          icon
+        >
+          <VBadge
+            :color="VColors.Primary"
+            dot
+          >
+            <VIcon
+              :icon="IconsList.BellIcon"
+              :size="20"
+            />
+          </VBadge>
+        </VBtn>
+      </template>
+    </VTooltip>
   </div>
 </template>
