4d3f8a72a89d209e26dbfbe8942c815ad955c839
Konstantin Khomenko <k.khomenko@upstars.com>
2025-11-21 10:38:25 +0200

    new api key

--- src/libs/froala.ts
+++ src/libs/froala.ts
@@ -7,4 +7,6 @@ import 'froala-editor/css/froala_style.min.css'
 // Import Froala Editor component
 import VueFroala from 'vue-froala-wysiwyg'
 
+export const FROALA_API_KEY = 'ZOD3gC7A6D6D6C3E3B2C-7SLJCKHXOSLMc2YGSGe1ZXHSa1CgC3E3G3G3B7D5B3F4C2A2=='
+
 export default VueFroala
