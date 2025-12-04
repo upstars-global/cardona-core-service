e86d632ff7c76d02da08c4d1f4efb8b089120b5a
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2025-12-04 16:26:02 +0200

    BAC-6670: Filter projects by selected product in ProjectSelect compon… (#349)
    * BAC-6670: Filter projects by selected product in ProjectSelect component.
    
    * BAC-6670: Add support for Marbella product across user module and VerticalNav component.
    
    * BAC-6670: Fix null safety issue when filtering projects by selected product in ProjectSelect component.
    
    * BAC-6670: Update productId type to support optional and string values in ProjectInfoInput
    
    * BAC-6670: add computed property to filter projects by selected product
    
    * BAC-6670: set request headers based on selected project in ProjectSelect component
    
    * BAC-6670: add watch functionality to ProjectSelect component for reactive updates
    
    * BAC-6670: update ProjectSelect component to use props for project filtering and enhance project selection logic

--- src/@layouts/components/ProjectSelect.vue
+++ src/@layouts/components/ProjectSelect.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, watch } from 'vue'
 import { useStore } from 'vuex'
 import { useFavicon } from '@vueuse/core'
+import type { ProjectInfo } from '../../@model/project'
 import { IconsList } from '../../@model/enums/icons'
 import { useChangeProject } from '../../composables/useChangeProject'
+import ApiService from '../../services/api'
+
+const props = defineProps<{
+  projects: ProjectInfo[]
+}>()
 
 const store = useStore()
 
@@ -14,13 +20,21 @@ const selectProject = computed({
   set: val => changeProject(val),
 })
 
-const projects = computed(() => store.getters.userInfo.projects)
-const cantSelect = computed(() => projects.value.length < 2)
+const cantSelect = computed(() => props.projects.length < 2)
 
 onMounted(() => {
   const faviconPath = store.getters.selectedProjectWithoutPriority?.iconPath || '/favicon.ico'
 
   useFavicon(faviconPath)
+})
+watch(selectProject, project => {
+  if (!project)
+    return
+  ApiService.setHeaders({
+    MarbellaProject: project?.originProject?.alias,
+  })
+}, {
+  immediate: true,
 })
 </script>
 
