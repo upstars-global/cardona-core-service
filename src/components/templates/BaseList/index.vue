37615b57c79f9a9d0b9aff034202586c2935e802
Vitalii <v.pecheritsya@upstars.com>
2025-12-08 16:59:00 +0200

    BAC-6674: refactor expose properties to use getters for improved reactivity

--- src/components/templates/BaseList/index.vue
+++ src/components/templates/BaseList/index.vue
@@ -43,14 +43,14 @@ const dynamicComponent = computed(() =>
 )
 
 defineExpose({
-  reFetchList: listByTypeRef.value?.reFetchList,
-  resetSelectedItem: listByTypeRef.value?.resetSelectedItem,
-  selectedItems: listByTypeRef.value?.selectedItems,
-  disableRowIds: listByTypeRef.value?.disableRowIds,
-  sortData: listByTypeRef.value?.sortData,
-  items: listByTypeRef.value?.items,
-  isSidebarShown: listByTypeRef.value?.isSidebarShown,
-  searchQuery: listByTypeRef.value?.searchQuery,
+  get reFetchList() { return listByTypeRef.value?.reFetchList },
+  get resetSelectedItem() { return listByTypeRef.value?.resetSelectedItem },
+  get selectedItems() { return listByTypeRef.value?.selectedItems },
+  get disableRowIds() { return listByTypeRef.value?.disableRowIds },
+  get sortData() { return listByTypeRef.value?.sortData },
+  get items() { return listByTypeRef.value?.items },
+  get isSidebarShown() { return listByTypeRef.value?.isSidebarShown },
+  get searchQuery() { return listByTypeRef.value?.searchQuery },
 })
 </script>
 
