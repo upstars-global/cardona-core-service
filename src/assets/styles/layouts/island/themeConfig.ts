542eaaef800059cbf8f99d9d03fc904cbd998b4f
Vitalii <v.pecheritsya@upstars.com>
2026-01-20 15:38:28 +0200

    BAC-7050: implement theme management with dark and light modes

---
+++ src/assets/styles/layouts/island/themeConfig.ts
@@ -0,0 +1,113 @@
+import type { ThemeDefinition } from 'vuetify'
+
+export const theme: { dark?: ThemeDefinition; light?: ThemeDefinition } = {
+  dark: {
+    colors: {
+      'primary': '#6D60F0',
+      'secondary': '#787A80',
+      'on-secondary': '#fff',
+      'success': '#0DB157',
+      'info': '#3B83F7',
+      'warning': '#F08C35',
+    },
+  },
+}
+
+// const colors = {
+//   backgrounds: {
+//     content: '#161822',
+//     disabled: '#16182200',
+//     generalA: '#13151F',
+//     generalB: '#0E1017',
+//     header: '#242532',
+//     invertA: '#F4F4F7',
+//     neutral: '#787A80',
+//   },
+//
+//   effects: {
+//     linearA: '#32313C',
+//     linearB: '#3A3A3D',
+//   },
+//
+//   states: {
+//     danger: {
+//       default: {
+//         active: '#EA3D3E',
+//         default: '#EA3D3E',
+//       },
+//       tonal: {
+//         active: '#21151B',
+//         default: '#21151B',
+//       },
+//     },
+//
+//     info: {
+//       default: {
+//         active: '#3B83F7',
+//         default: '#3B83F7',
+//       },
+//       tonal: {
+//         active: '#121824',
+//         default: '#131B2A',
+//       },
+//     },
+//
+//     primary: {
+//       default: {
+//         active: '#6D60F0',
+//         default: '#6D60F0',
+//       },
+//       tonal: {
+//         active: '#161829',
+//         default: '#161829',
+//       },
+//     },
+//
+//     secondary: {
+//       default: {
+//         active: '#787A80',
+//         default: '#787A80',
+//       },
+//       tonal: {
+//         active: '#171A20',
+//         default: '#171A20',
+//       },
+//     },
+//
+//     success: {
+//       default: {
+//         active: '#0DB157',
+//         default: '#0DB157',
+//       },
+//       tonal: {
+//         active: '#0F1F1D',
+//         default: '#0F1F1D',
+//       },
+//     },
+//
+//     warning: {
+//       default: {
+//         active: '#F08C35',
+//         default: '#F08C35',
+//       },
+//       tonal: {
+//         active: '#211C1A',
+//         default: '#211C1A',
+//       },
+//     },
+//   },
+//
+//   // text: {
+//   //   disabled: '#494B5A',
+//   //   error: '#F97171',
+//   //   general: '#D5D5DE',
+//   //   info: '#3B83F7',
+//   //   invert: '#343246',
+//   //   link: '#A69FF8',
+//   //   primary: '#A69FF8',
+//   //   secondary: '#7D7E85',
+//   //   success: '#0DB157',
+//   //   warning: '#F08C35',
+//   //   white: '#FFFFFF',
+//   // },
+// }
--- src/composables/useLayoutChanger.ts
+++ src/composables/useLayoutChanger.ts
@@ -2,12 +2,34 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import LayoutDefault from '../layouts/default.vue'
 import { CoreLayouts } from '../@model/layouts'
+import { useThemeChanger } from '../composables/useThemeChanger'
+import { theme as islandTheme } from '../assets/styles/layouts/island/themeConfig'
 
 export function useLayoutChanger(layoutByMeta: Record<string, Component>, defaultLayout = CoreLayouts.Default) {
   const route = useRoute()
 
   const layoutKey = computed(() => route.meta?.layout || defaultLayout)
   const activeLayout = computed(() => layoutByMeta[layoutKey.value] || LayoutDefault)
+
+  const themeChanger = useThemeChanger()
+
+  const themesKeysByLayout = {
+    [CoreLayouts.Island]: islandTheme,
+  }
+
+  watch(() => layoutKey.value, layout => {
+    const actualTheme = themesKeysByLayout[layout] ?? null
+
+    if (!actualTheme)
+      return
+
+    Object.keys(actualTheme).forEach(key => {
+      themeChanger.patchThemeByName(key, themesKeysByLayout[layout][key])
+    })
+  })
+
+  if (layoutKey.value === CoreLayouts.Island)
+    themeChanger.patchThemeByName('dark', islandTheme)
 
   return {
     activeLayout,
