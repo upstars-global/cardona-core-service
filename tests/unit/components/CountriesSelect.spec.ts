eaf6335998afad1f3a54f0e338cf8cf0dee6060d
Vitalii <v.pecheritsya@upstars.com>
2026-01-30 20:50:15 +0200

    BAC-7282: add tests for region selection logic in CountriesSelect component

--- tests/unit/components/CountriesSelect.spec.ts
+++ tests/unit/components/CountriesSelect.spec.ts
@@ -1,17 +1,12 @@
-// import { describe, expect, it } from 'vitest'
-import { beforeEach, describe, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { cloneDeep } from 'lodash'
 import { nextTick } from 'vue'
+import { flushPromises } from '@vue/test-utils'
 import CountriesSelect from '../../../src/components/CountriesSelect.vue'
 
-// import {useRegionsStore} from "../../../src/stores/regions";
-/// import { mockModal } from '../mocks/modal-provide-config'
 import { setMountComponent } from '../utils'
 import { mockModal } from '../mocks/modal-provide-config'
 import { regionFetchListResultMock } from '../mocks/country-select'
-import {flushPromises} from "@vue/test-utils";
-
-// import { testOn } from '../templates/shared-tests/test-case-generator'
 
 export const modalSpy = vi.spyOn(mockModal, 'showModal')
 
@@ -34,7 +29,22 @@ const defaultProps = {
 
 let props
 
-// const fetchRegionListMock = vi.fn(async () => [])
+const getExpectedSelectedRegions = (countryOrRegion: string) => {
+  const result = []
+
+  for (const key in regionFetchListResultMock) {
+    if (!countryOrRegion.includes('')) {
+      if (countryOrRegion === key)
+        result.push(key)
+    }
+    else {
+      if (key.includes(countryOrRegion))
+        result.push(key)
+    }
+  }
+
+  return result
+}
 
 const fetchRegionListMock = vi.fn().mockResolvedValue(regionFetchListResultMock)
 
@@ -48,6 +58,24 @@ vi.mock('../../../src/stores/regions', () => {
     }),
   }
 })
+
+const testOnValidEmittedValue = async (value: string[]) => {
+  const wrapper = getMountComponent(props)
+
+  await flushPromises()
+
+  ;(wrapper.vm as any).onSelectItem((regionFetchListResultMock as any)['UA'])
+
+  await nextTick()
+
+  const emitted = wrapper.emitted('update:modelValue') || []
+  const emittedValue = emitted[0][0] as string[]
+
+  const expectedValue = getExpectedSelectedRegions('UA')
+
+  expect(emittedValue).toHaveLength(expectedValue.length)
+  expect(emittedValue).toEqual(expect.arrayContaining(expectedValue))
+}
 
 describe('CountriesSelect.spec', () => {
   beforeEach(() => {
@@ -66,22 +94,11 @@ describe('CountriesSelect.spec', () => {
     }
   })
 
-  it('Renders BaseModal and displays modal with correct slot content after showModal is called', async () => {
-    props.modelValue = []
+  it('Expect selecting Ukraine country selects all its regions', async () => {
+    await testOnValidEmittedValue(['UA'])
+  })
 
-    const wrapper = getMountComponent(props)
-
-    await flushPromises()
-
-    ;(wrapper.vm as any).onSelectItem((regionFetchListResultMock as any)['UA'])
-
-    await nextTick()
-
-    const emitted = wrapper.emitted('update:modelValue') || []
-    console.log(emitted)
-    // expect(emitted.length).toBeGreaterThan(0)
-    //
-    // const last = emitted[emitted.length - 1][0]
-    // expect(last).toEqual(expect.arrayContaining(['UA-KY']))
+  it('Expect selecting Poland-Mazovia region selects only this region', async () => {
+    await testOnValidEmittedValue(['PL-MZ'])
   })
 })
