50d1e9e476b11de1d1febe42fcea9821c502275c
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2026-02-02 11:43:25 +0200

    Bac 7282 country test (#390)
    * BAC-7282: add unit tests for CountriesSelect component and mock region data
    
    * BAC-7282: add tests for region selection logic in CountriesSelect component
    
    * BAC-7282: enhance CountriesSelect component with additional tests and data attributes
    
    * BAC-7282: remove commented code and streamline region selection logic

--- src/components/CountriesSelect.vue
+++ src/components/CountriesSelect.vue
@@ -172,7 +172,8 @@ const onSelectItem = (region: RegionInfo) => {
       selectedCountriesVisible.value.set(key, sortBy(selectedRegions, r => r.code))
     }
   }
-  selectRef.value.clearSelection()
+
+  selectRef.value?.clearSelection?.()
   updateValue()
 }
 
@@ -205,6 +206,7 @@ const keyID = Math.random()
 
 defineExpose({
   selectedCountriesVisibleView,
+  onSelectItem,
 })
 </script>
 
@@ -214,6 +216,7 @@ defineExpose({
       <hr class="mb-6 mt-0">
       <VBtnToggle
         v-model="countriesRadioModel"
+        data-test-id="toggle-mode-countries"
         :disabled="disabled"
         divided
         mandatory
@@ -235,7 +238,10 @@ defineExpose({
 
     <div class="mt-4">
       <div class="mb-1 field-generator-label text-body-2 text-high-emphasis d-flex justify-space-between align-baseline">
-        <span :class="{ required }">
+        <span
+          :class="{ required }"
+          data-test-id="countries-select-label"
+        >
           {{ customLabel ? customLabel : countriesRadioLabel }}
         </span>
 
@@ -275,6 +281,7 @@ defineExpose({
     <span
       v-if="!required"
       class="text-sm text-color-mute mt-1"
+      data-test-id="countries-select-description"
     >
       {{ customDescription || $t('component.countriesSelect.description') }}
     </span>
