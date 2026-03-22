653344b29c3059151d594c906e18514e6a6cee82
Konstantin Khomenko <k.khomenko@upstars.com>
2026-03-22 19:29:41 +0200

    baseListSelection store

--- src/stores/baseListSelection.ts
+++ src/stores/baseListSelection.ts
@@ -2,10 +2,26 @@ import { defineStore } from 'pinia'
 
 export const useBaseListSelection = defineStore('baseListSelection', {
   state: () => ({
-    selectedIds: new Set(),
+    selectedIds: new Set<string>(),
   }),
   getters: {
-
+    getAllSelectedIds: ({ selectedIds }) => selectedIds,
   },
-  actions: {},
+  actions: {
+    syncPageSelection(pageIds: string[], selectedPageIds: string[]) {
+      const newSelectedIds = new Set(this.selectedIds)
+
+      for (const id of pageIds)
+        newSelectedIds.delete(id)
+
+      for (const id of selectedPageIds)
+        newSelectedIds.add(id)
+
+      this.selectedIds = newSelectedIds
+    },
+
+    clearSelectedIds() {
+      this.selectedIds = new Set()
+    },
+  },
 })
