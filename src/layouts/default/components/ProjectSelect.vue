817446e16456de2a2af8d1229c9db20771bbb3e9
SimpllleUser <pecheritsa.vv@gmail.com>
2026-03-30 12:29:46 +0300

    BAC-7566: add ProjectSelect component for project selection and update import path in VerticalNav

---
+++ src/layouts/default/components/ProjectSelect.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+import { computed, watch } from 'vue'
+import { useFavicon } from '@vueuse/core'
+import type { ProjectInfo } from '../../@model/project'
+import { IconsList } from '../../@model/enums/icons'
+import { useChangeProject } from '../../composables/useChangeProject'
+import { useUserStore } from '../../stores/user'
+import ApiService from '../../services/api'
+
+const props = defineProps<{
+  projects: ProjectInfo[]
+}>()
+
+const userStore = useUserStore()
+
+const { changeProject } = useChangeProject()
+
+const selectProject = computed({
+  get: () => userStore.selectedProjectWithoutPriority,
+  set: val => changeProject(val),
+})
+
+const cantSelect = computed(() => props.projects.length < 2)
+const isMarbella = computed(() => userStore.isMarbella)
+
+watch(() => selectProject.value.id, () => {
+  const faviconPath = userStore.selectedProjectWithoutPriority?.iconPath || '/favicon.ico'
+
+  useFavicon(faviconPath)
+}, { immediate: true })
+
+watch(() => selectProject.value, project => {
+  if (!project || !isMarbella.value)
+    return
+  ApiService.setHeaders({
+    MarbellaProject: selectProject.value.alias,
+  })
+}, {
+  immediate: true,
+})
+</script>
+
+<template>
+  <div
+    class="project-select"
+    :class="{ 'cant-select': cantSelect }"
+  >
+    <VueSelect
+      v-model="selectProject"
+      :options="projects"
+      label="publicName"
+      class="select-field select-field-color"
+      :clearable="false"
+      :searchable="false"
+      :no-drop="cantSelect"
+    >
+      <template #selected-option="{ publicName, title, alias, iconPath }">
+        <div class="d-flex align-center overflow-hidden gap-2">
+          <img
+            v-if="iconPath"
+            :alt="alias"
+            :src="iconPath"
+            class="project-logo"
+          >
+          <div
+            v-else
+            class="project-logo project-logo-no-ico d-flex align-center justify-center select-field-color"
+          >
+            {{ publicName[0].toUpperCase() }}
+          </div>
+          <span class="text-expanded select-field-color">{{ publicName || title }}</span>
+        </div>
+      </template>
+      <template #option="{ publicName, title, alias, iconPath }">
+        <div class="d-flex  align-center overflow-hidden gap-2">
+          <img
+            v-if="iconPath"
+            :alt="alias"
+            :src="iconPath"
+            class="project-logo"
+          >
+          <div
+            v-else
+            class="project-logo project-logo-no-ico d-flex align-center justify-center select-field-color"
+          >
+            {{ publicName[0].toUpperCase() }}
+          </div>
+          <span class="text-expanded">{{ publicName || title }}</span>
+          <VIcon
+            v-if="selectProject.publicName === publicName"
+            :icon="IconsList.CheckIcon"
+            class="ml-auto"
+          />
+        </div>
+      </template>
+      <template #open-indicator="{ attributes }">
+        <div
+          v-if="!cantSelect"
+          v-bind="attributes"
+        >
+          <VIcon
+            :icon="IconsList.ChevronDownIcon"
+            class="text-expanded select-field-color"
+          />
+        </div>
+      </template>
+    </VueSelect>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.project-select {
+  :deep(.vs__selected-options) {
+    flex-wrap: nowrap;
+
+    .vs__search {
+      flex: 0 0 0;
+      padding: 0;
+
+      &:focus {
+        padding: 0;
+      }
+    }
+  }
+
+  :deep(.vs__dropdown-menu) {
+    min-width: auto;
+  }
+
+  &--collapsed {
+    :deep(.vs__actions) {
+      display: none;
+    }
+    .text-expanded {
+      display: none;
+    }
+    :deep(.vs__dropdown-option) {
+      padding-left: 0.875rem;
+      padding-right: 0.875rem;
+    }
+  }
+
+  .select-field-color {
+    color: rgba(var(--v-theme-grey-900), var(--v-body-opacity));
+  }
+}
+.project-logo {
+  width: 1.5rem;
+  height: 1.5rem;
+  min-width: 1.5rem;
+  min-height: 1.5rem;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-top: 2px;
+  margin-bottom: 2px;
+}
+.project-logo-no-ico {
+  background: rgb(var(--v-theme-primary));
+  color: white;
+}
+</style>
