156d423157f54beceafdfd44dd57f82f04659407
SimpllleUser <pecheritsa.vv@gmail.com>
2026-04-05 21:44:29 +0300

    new_layout_test: enhance sidebar layout and improve navigation item styling

--- src/layouts/custom/index.vue
+++ src/layouts/custom/index.vue
@@ -34,61 +34,80 @@ const isCollapsed = computed(() => rail.value && !isHovered.value)
       @mouseenter="isHovered = true"
       @mouseleave="isHovered = false"
     >
-      <div class="px-4 pt-4 pb-2 d-flex align-center justify-space-between">
-        <ProductsSelect :is-collapsed-menu="isCollapsed" />
-        <VIcon
-          v-show="!isCollapsed"
-          class="text-secondary"
-          :icon="!rail ? IconsList.CircleDotIcon : IconsList.CircleIcon"
-          @click="rail = !rail"
-        />
+      <div class="sidebar-inner d-flex flex-column h-100">
+        <!-- Fixed top -->
+        <div class="sidebar-top">
+          <div class="px-4 pt-4 pb-2 d-flex align-center justify-space-between">
+            <ProductsSelect :is-collapsed-menu="isCollapsed" />
+            <VIcon
+              v-show="!isCollapsed"
+              class="text-secondary"
+              :icon="!rail ? IconsList.CircleDotIcon : IconsList.CircleIcon"
+              @click="rail = !rail"
+            />
+          </div>
+
+          <div
+            v-if="userStore.selectedProjectWithoutPriority"
+            class="px-4 pb-3"
+          >
+            <ProjectSelect
+              :projects="projects"
+              :is-collapsed="isCollapsed"
+            />
+          </div>
+        </div>
+
+        <!-- Scrollable nav items -->
+        <div class="sidebar-nav flex-grow-1 overflow-y-auto">
+          <div
+            v-if="isCollapsed"
+            class="d-flex flex-column align-center py-1 gap-1"
+          >
+            <VBtn
+              v-for="item in navItems"
+              :key="item.title"
+              :icon="item.icon?.icon"
+              :to="item.to"
+              variant="text"
+              rounded="lg"
+              color="white"
+              active-color="primary"
+              size="40"
+            />
+          </div>
+
+          <VList
+            v-else
+            nav
+            bg-color="transparent"
+            class="py-1 bg-sidebar"
+          >
+            <VListItem
+              v-for="item in navItems"
+              :key="item.title"
+              :title="t(item.title)"
+              :to="item.to"
+              rounded="lg"
+              active-color="primary"
+              base-color="white"
+              class="bg-sidebar"
+            >
+              <template #prepend>
+                <VIcon
+                  :icon="item.icon?.icon"
+                  color="white"
+                  class="me-2"
+                />
+              </template>
+            </VListItem>
+          </VList>
+        </div>
+
+        <div class="sidebar-bottom">
+          <CustomMenu :is-collapsed-menu="isCollapsed" />
+        </div>
       </div>
-
-      <div
-        v-if="userStore.selectedProjectWithoutPriority"
-        class="px-4 pb-3"
-      >
-        <ProjectSelect
-          :projects="projects"
-          :is-collapsed="isCollapsed"
-        />
-      </div>
-
-      <div
-        v-if="isCollapsed"
-        class="d-flex flex-column align-center py-1 gap-1"
-      >
-        <VBtn
-          v-for="item in navItems"
-          :key="item.title"
-          :icon="item.icon?.icon"
-          :to="item.to"
-          variant="text"
-          rounded="lg"
-          active-color="primary"
-          size="40"
-        />
-      </div>
-
-      <VList
-        v-else
-        nav
-        class="py-1"
-      >
-        <VListItem
-          v-for="item in navItems"
-          :key="item.title"
-          :prepend-icon="item.icon?.icon"
-          :title="t(item.title)"
-          :to="item.to"
-          rounded="lg"
-          active-color="primary"
-        />
-      </VList>
-
-      <template #append>
-        <CustomMenu :is-collapsed-menu="isCollapsed" />
-      </template>
     </VNavigationDrawer>
 
     <VAppBar
@@ -108,9 +127,9 @@ const isCollapsed = computed(() => rail.value && !isHovered.value)
         </VBtn>
       </div>
     </VAppBar>
-    <VMain>
-      <div class="bg-sidebar w-100 h-100 pl-2 pr-2 pb-2 overflow-auto">
-        <div class="bg-surface w-100 h-100 layout-border-bottom">
+    <VMain style="height: 100dvh; overflow: hidden;">
+      <div class="bg-sidebar w-100 h-100 pl-2 pr-2 pb-2 d-flex flex-column">
+        <div class="bg-surface flex-grow-1 layout-border-bottom overflow-y-auto">
           <VContainer
             fluid
             class="px-0"
@@ -145,6 +164,31 @@ const isCollapsed = computed(() => rail.value && !isHovered.value)
 
 .custom-layout :deep(.v-navigation-drawer) {
   border-inline-end: none !important;
+}
+
+.custom-layout :deep(.v-navigation-drawer__content) {
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar-inner {
+  min-height: 0;
+}
+
+.sidebar-nav {
+  min-height: 0;
+}
+
+.custom-layout :deep(.v-navigation-drawer .v-list-item-title),
+.custom-layout :deep(.v-navigation-drawer .v-list-item__prepend .v-icon) {
+  color: white !important;
+}
+
+.custom-layout :deep(.v-navigation-drawer .v-list-item--active .v-list-item__overlay),
+.custom-layout :deep(.v-navigation-drawer .v-btn--active .v-btn__overlay) {
+  background-color: rgba(255, 255, 255, 0.06) !important;
+  opacity: 1 !important;
 }
 
   .layout-border-top {
