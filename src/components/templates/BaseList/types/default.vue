d7036482955d2d3dad87dad151a02a8796fccb70
SimpllleUser <pecheritsa.vv@gmail.com>
2026-04-29 16:31:47 +0300

    BAC-7815: update inline filter class for responsive width adjustment

--- src/components/templates/BaseList/types/default.vue
+++ src/components/templates/BaseList/types/default.vue
@@ -734,6 +734,9 @@ defineExpose({ reFetchList, resetSelectedItem, selectedItems, disableRowIds, sor
         <div
           v-if="config.inlineFilters?.isNotEmpty"
           class="w-auto d-flex align-center justify-content-start"
+          :class="{
+            'w-auto': config.withSearch,
+            'w-100': !config.withSearch}"
         >
           <InlineFilters
             :filter-fields="filterFields"
