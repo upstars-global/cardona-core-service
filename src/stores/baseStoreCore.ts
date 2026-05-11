3b0c7cbb0493db2b658ddf30e7f3c856ec828d51
SimpllleUser <pecheritsa.vv@gmail.com>
2026-05-11 17:14:14 +0300

    BAC-7814: refactor filter handling to improve project alias integration

--- src/stores/baseStoreCore.ts
+++ src/stores/baseStoreCore.ts
@@ -36,17 +36,17 @@ export const transformNameToType = (type: string): string =>
 // @ts-expect-error
 const isNeocoreProduct = productName === productsName.neocore
 
-const combineFilter = (
-  filters: Record<string, any> = {},
-  projectAlias?: string,
-): Record<string, any> | undefined => {
-  const filter = {
-    ...filters,
-    project: isNeocoreProduct ? projectAlias : undefined,
-  }
-
-  return Object.values(filter).some(v => !isUndefined(v)) ? filter : undefined
-}
+// const combineFilter = (
+//   filters: Record<string, any> = {},
+//   projectAlias?: string,
+// ): Record<string, any> | undefined => {
+//   const filter = {
+//     ...filters,
+//     project: isNeocoreProduct ? projectAlias : undefined,
+//   }
+//
+//   return Object.values(filter).some(v => !isUndefined(v)) ? filter : undefined
+// }
 
 export const useBaseStoreCore = defineStore('baseStoreCore', {
   actions: {
@@ -66,7 +66,7 @@ export const useBaseStoreCore = defineStore('baseStoreCore', {
           perPage: payload.data.perPage ?? 10,
         },
         sort: payload.data.sort,
-        filter: combineFilter(payload.data.filter, this.$selectedProjectAlias),
+        filter: payload.data.filter,
       })
 
       return new ListData(response, payload.options.listItemModel)
@@ -84,7 +84,7 @@ export const useBaseStoreCore = defineStore('baseStoreCore', {
             pageNumber: payload.data.page ?? 1,
             perPage: payload.data.perPage,
           },
-          data: combineFilter(payload.data.filter, this.$selectedProjectAlias),
+          data: payload.data?.filter,
         },
         {
           withSuccessToast: true,
