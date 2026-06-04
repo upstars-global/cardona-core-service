d571bf7e9dbc41ff3f5573d2e7a65f6048c67039
SimpllleUser <pecheritsa.vv@gmail.com>
2026-06-04 15:35:30 +0300

    BAC-7653: enhance Storybook integration with new route and update build output path

---
+++ src/pages/storybook/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+definePage({ name: 'storybook' })
+</script>
+
+<template>
+  <div class="storybook-wrapper">
+    <iframe
+      src="/storybook/index.html"
+      class="storybook-frame"
+      title="Component Library"
+    />
+  </div>
+</template>
+
+<style scoped>
+.storybook-wrapper {
+  height: calc(100vh - 64px);
+}
+
+.storybook-frame {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
