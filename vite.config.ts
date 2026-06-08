de9e25056f298e8a034e5235bac7c999c6fbaa3c
SimpllleUser <pecheritsa.vv@gmail.com>
2026-06-08 12:50:18 +0300

    BAC-8096: add deduplication for countries-list in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -112,6 +112,7 @@ export default defineConfig({
   },
   define: { 'process.env': {} },
   resolve: {
+    dedupe: ['countries-list'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@themeConfig': fileURLToPath(new URL('./themeConfig.ts', import.meta.url)),
