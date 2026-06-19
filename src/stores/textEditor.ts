a21a78b7e60c590cc161910a1489d548ce4834ab
SimpllleUser <pecheritsa.vv@gmail.com>
2026-06-19 16:13:29 +0300

    BAC-8035: add gift types and implement gift fetching methods in text editor store

--- src/stores/textEditor.ts
+++ src/stores/textEditor.ts
@@ -1,5 +1,29 @@
 import { defineStore } from 'pinia'
 import { omit } from 'lodash'
+import type { OptionsItem } from '../@model'
+import { ListData } from '../@model'
+import { convertDictionaryToOptions } from '../helpers'
+import i18n from '../plugins/i18n'
+import ApiService from '../services/api'
+
+export class GiftOptionsType implements OptionsItem {
+  readonly id: string
+  readonly name: string
+
+  constructor(data: OptionsItem) {
+    this.id = data.name
+    this.name = i18n.t(`common.giftTypes.${data.name}`)
+  }
+}
+export class GiftOptionsItem {
+  readonly id: string
+  readonly name: string
+
+  constructor(data: OptionsItem) {
+    this.id = data.name
+    this.name = data.name
+  }
+}
 
 export const useTextEditorStore = defineStore('textEditor', {
   state: () => ({
@@ -26,6 +50,37 @@ export const useTextEditorStore = defineStore('textEditor', {
     },
     removeVariableValueByKey(key) {
       this.variableTextBuffer = omit(this.variableTextBuffer, [key])
+    },
+    async fetchGiftsTypeList() {
+      const { data: responseData }: { data: Record<string, string> } = await ApiService.request({
+        type: 'App.V2.Gifts.Types.List',
+        data: {
+          project: this.$selectedProjectAlias,
+        },
+      })
+
+      const data: Array<OptionsItem> = convertDictionaryToOptions(responseData)
+
+      return new ListData<OptionsItem>({ data }, GiftOptionsType)
+    },
+    async fetchGifts(parmas?: { pageNumber?: number; perPage?: number; filter?: Record<string, any> }) {
+      return new ListData<string>(
+        await ApiService.request({
+          type: 'App.V2.Gifts.List',
+          data: {
+            project: this.$selectedProjectAlias,
+          },
+          pagination: {
+            pageNumber: parmas?.pageNumber || 1,
+            perPage: parmas?.perPage || 50,
+          },
+          filter: {
+            ...(parmas?.filter || {}),
+            project: this.$selectedProjectAlias,
+          },
+        }),
+        GiftOptionsItem,
+      )
     },
   },
 })
