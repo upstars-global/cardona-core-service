93b430a3dd17c96d0b80a3858e08861828446df4
SimpllleUser <pecheritsa.vv@gmail.com>
2026-08-05 14:54:31 +0300

    BAC-7722: adjust padding in layout appbar for better alignment

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -118,7 +118,7 @@ const contentScrollEl = ref<HTMLElement | null>(null)
     <VMain class="v-main--fullscreen">
       <div class="bg-sidebar w-100 h-100 pr-2 pb-2 d-flex flex-column">
         <div class="layout-appbar pt-2 d-flex flex-column">
-          <div class="flex-grow-1 d-flex align-center px-4 bg-surface layout-border-top">
+          <div class="flex-grow-1 d-flex align-center pt-4 px-4 bg-surface layout-border-top">
             <AppBreadcrumb class="flex-grow-1" />
             <VBtn
               icon
