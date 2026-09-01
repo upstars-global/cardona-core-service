ce982319dbae05379af1e2a5743dd4613e6b0b00
Vitalii Pecheritsya <v.pecheritsya@upstars.com>
2026-09-01 16:27:59 +0300

    BAC-8559: add reset state functionality to Pinia store and update aut… (#547)
    * BAC-8559: add reset state functionality to Pinia store and update auth handling
    
    * BAC-8559: rename router and pinia plugin files for consistency
    
    * BAC-8559: refactor state management by renaming reset state plugin and implementing initialization logic
    
    * BAC-8559: update import path for RESET_SKIP_STORES in index.ts
    
    * BAC-8559: refactor state management by renaming useInitState to resetStatePlugin and updating import paths
    
    * BAC-8559: update router import path for consistency
    
    * BAC-8559: update RESET_SKIP_STORES to use consistent naming convention
    
    * BAC-8559: remove commented-out code for resetting stores in authCore
    
    * BAC-8559: add path alias documentation for developer projects
    
    ---------
    
    Co-authored-by: SimpllleUser <pecheritsa.vv@gmail.com>
    Co-authored-by: Олег Мига <o.miha@upstars.com>

--- src/plugins/1.pinia.ts
+++ src/plugins/1.pinia.ts
@@ -1,8 +1,10 @@
 import { createPinia } from 'pinia'
 import type { App } from 'vue'
 import { useUserPlugin } from '../stores/plugins/userPlugin'
+import { resetStatePlugin } from '../stores/plugins/resetStatePlugin'
 
 export const store = createPinia()
+store.use(resetStatePlugin)
 store.use(useUserPlugin)
 
 export default function (app: App) {
